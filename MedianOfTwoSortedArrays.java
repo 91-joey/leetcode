abbce895008ddbde4ff7197c06d0b72f634529f9
joeyLocal <joey.91@qq.com>
2022-11-05 17:01:57 +0800

    4.寻找两个正序数组的中位数

--- MedianOfTwoSortedArrays.java
+++ MedianOfTwoSortedArrays.java
@@ -45,7 +45,12 @@ import java.util.Arrays;
 public class MedianOfTwoSortedArrays {
     public static void main(String[] args) {
         Solution solution = new MedianOfTwoSortedArrays().new Solution();
-        System.out.println(solution.findMedianSortedArrays(new int[]{0, 0, 0, 0, 0}, new int[]{-1, 0, 0, 0, 0, 0, 1}));
+//        System.out.println(solution.findMedianSortedArrays(new int[]{0, 0, 0, 0, 0}, new int[]{-1, 0, 0, 0, 0, 0, 1}));
+//        System.out.println(solution.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
+//        System.out.println(solution.findMedianSortedArrays(new int[]{1, 2}, new int[]{3}));
+        System.out.println(solution.findMedianSortedArrays(new int[]{1, 2, 3}, new int[]{4}));
+//        System.out.println(solution.findMedianSortedArrays(new int[]{2}, new int[]{}));
+//        System.out.println(solution.findMedianSortedArrays(new int[]{1,3}, new int[]{2,7}));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -64,7 +69,7 @@ public class MedianOfTwoSortedArrays {
                     (merged[mid - 1] + merged[mid]) / 2.0;
         }
 
-        //双指针排序 m+n
+        //双指针排序  m+n    m+n
         public double findMedianSortedArrays8(int[] nums1, int[] nums2) {
             int size = nums1.length + nums2.length;
             int[] merged = new int[size];
@@ -87,19 +92,89 @@ public class MedianOfTwoSortedArrays {
                     (merged[mid - 1] + merged[mid]) / 2.0;
         }
 
+        //双指针（不排序）  m+n>>1  1
+        public double findMedianSortedArrays7(int[] nums1, int[] nums2) {
+            int size = nums1.length + nums2.length;
+            int i = 0, j = 0, first = 0, second = 0;
+            int mid = size >> 1;
+            while (i + j <= mid) {
+                first = second;
+                if (j >= nums2.length || (i < nums1.length && nums1[i] <= nums2[j]))
+                    second = nums1[i++];
+                else
+                    second = nums2[j++];
+            }
+            return (size & 1) == 1 ?
+                    second :
+                    (first + second) / 2.0;
+        }
+
+        //二分    log((m+n)/2)  1
+        public double findMedianSortedArrays6(int[] nums1, int[] nums2) {
+            int m = nums1.length;
+            int n = nums2.length;
+            int second = (m + n >> 1) + 1;
+            double secondTh = getKth(nums1, 0, m - 1, nums2, 0, n - 1, second);
+            return (((m + n) & 1) == 1) ?
+                    secondTh :
+                    (getKth(nums1, 0, m - 1, nums2, 0, n - 1, second - 1) + secondTh) * 0.5;
+        }
+
+        private double getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+            int len1 = end1 - start1 + 1;
+            int len2 = end2 - start2 + 1;
+            if (len1 == 0) return nums2[start2 + k - 1];
+            else if (len2 == 0) return nums1[start1 + k - 1];
+            if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+
+            int i = start1 + Math.min(k >> 1, len1) - 1;
+            int j = start2 + Math.min(k >> 1, len2) - 1;
+            if (nums1[i] < nums2[j])
+                return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+            else
+                return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+        }
+
+        //自解二分（乱七八糟）    log(m+n)    1
         public double findMedianSortedArrays(int[] nums1, int[] nums2) {
             int m = nums1.length;
             int n = nums2.length;
-            int target = (m + n >> 1) + 1;
-            int l = 0, r = Math.max(m - 1, target);
+            if (m < n) return findMedianSortedArrays8(nums2, nums1);
+            if (n == 0) return (m & 1) == 1 ?
+                    nums1[m >> 1] :
+                    (nums1[(m >> 1) - 1] + nums1[m >> 1]) / 2.0;
+
+            int target = m + n >> 1;
+            int l = m - n >> 1, r = Math.min(m - 1, target);
+            double median = getMedian(nums1, nums2, m, n, target, l, r);
+            if (median != Double.MIN_VALUE) return median;
+            l = 0;
+            r = n - 1;
+            return getMedian(nums2, nums1, m, n, target, l, r);
+        }
+
+        private double getMedian(int[] nums1, int[] nums2, int m, int n, int target, int l, int r) {
             while (l <= r) {
                 int mid = l + r >> 1;
-                //target-mid=(m+n>>1)+1-mid  [(m+n>>1)-m+2,(m+n>>1)+1]
-                if ((nums2[target - mid] <= nums1[mid] && nums2[target - mid + 1] >= nums1[mid]) ||
-                        (nums2[target - mid] >= nums1[mid] && nums2[target - mid - 1] <= nums1[mid]))
-                    return 0;
+                if (target - mid < nums2.length && nums2[target - mid] < nums1[mid])
+                    r = mid - 1;
+                else if (target - mid - 1 >= 0 && nums2[target - mid - 1] > nums1[mid])
+                    l = mid + 1;
+                else {
+                    if (((m + n) & 1) == 0) {
+                        int neighbor;
+                        if (mid == 0)
+                            neighbor = nums2[target - mid - 1];
+                        else if (target - mid == 0)
+                            neighbor = nums1[mid - 1];
+                        else
+                            neighbor = Math.max(nums1[mid - 1], nums2[target - mid - 1]);
+                        return (nums1[mid] + neighbor) / 2.0;
+                    } else
+                        return nums1[mid];
+                }
             }
-            return 0;
+            return Double.MIN_VALUE;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
