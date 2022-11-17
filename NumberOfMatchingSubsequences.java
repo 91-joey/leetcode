9de16d61a3959e253f7119217a46e9be6f35c3a6
joeyLocal <joey.91@qq.com>
2022-11-17 14:37:06 +0800

    392.判断子序列

---
+++ NumberOfMatchingSubsequences.java
@@ -0,0 +1,149 @@
+//<p>给定字符串 <code>s</code>&nbsp;和字符串数组&nbsp;<code>words</code>, 返回&nbsp;&nbsp;<em><code>words[i]</code>&nbsp;中是<code>s</code>的子序列的单词个数</em>&nbsp;。</p>
+//
+//<p>字符串的 <strong>子序列</strong> 是从原始字符串中生成的新字符串，可以从中删去一些字符(可以是none)，而不改变其余字符的相对顺序。</p>
+//
+//<ul> 
+// <li>例如， <code>“ace”</code> 是 <code>“abcde”</code> 的子序列。</li> 
+//</ul>
+//
+//<p>&nbsp;</p>
+//
+//<p><strong>示例 1:</strong></p>
+//
+//<pre>
+//<strong>输入:</strong> s = "abcde", words = ["a","bb","acd","ace"]
+//<strong>输出:</strong> 3
+//<strong>解释:</strong> 有三个是&nbsp;s 的子序列的单词: "a", "acd", "ace"。
+//</pre>
+//
+//<p><strong>Example 2:</strong></p>
+//
+//<pre>
+//<strong>输入: </strong>s = "dsahjpjauf", words = ["ahjpjau","ja","ahbwzgqnuk","tnmlanowax"]
+//<strong>输出:</strong> 2
+//</pre>
+//
+//<p>&nbsp;</p>
+//
+//<p><strong>提示:</strong></p>
+//
+//<ul> 
+// <li><code>1 &lt;= s.length &lt;= 5 * 10<sup>4</sup></code></li> 
+// <li><code>1 &lt;= words.length &lt;= 5000</code></li> 
+// <li><code>1 &lt;= words[i].length &lt;= 50</code></li> 
+// <li><code>words[i]</code>和 <font color="#c7254e" face="Menlo, Monaco, Consolas, Courier New, monospace"><span style="font-size: 12.6px; background-color: rgb(249, 242, 244);">s</span></font>&nbsp;都只由小写字母组成。</li> 
+//</ul> 
+//<span style="display:block"><span style="height:0px"><span style="position:absolute">​​​​</span></span></span>
+//
+//<div><li>👍 243</li><li>👎 0</li></div>
+package org.example.leetcode.problems;
+
+import java.util.*;
+
+//792.匹配子序列的单词数
+//开题时间：2022-11-17 08:32:24
+public class NumberOfMatchingSubsequences {
+    public static void main(String[] args) {
+        Solution solution = new NumberOfMatchingSubsequences().new Solution();
+        System.out.println(solution.numMatchingSubseq("abcde", new String[]{"a", "bb", "acd", "ace"}));
+        System.out.println(-1 >> 1);
+        System.out.println(-7 >>> 1);
+        System.out.println(-1 / 2);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numMatchingSubseq9(String s, String[] words) {
+            Map<Integer, List<Integer>> char2indices = new HashMap<>();
+            for (int i = 'a'; i <= 'z'; i++)
+                char2indices.put(i, new ArrayList<>());
+
+            for (int i = 0; i < s.length(); i++)
+                char2indices.get((int) s.charAt(i)).add(i);
+
+            int cnt = 0;
+            out:
+            for (String word : words) {
+                for (int i = 0, idx = -1; i < word.length(); i++) {
+                    List<Integer> list = char2indices.get((int) word.charAt(i));
+                    if (list.isEmpty())
+                        continue out;
+                    int l = 0, r = list.size() - 1;
+                    while (l < r) {
+                        int mid = l + r >> 1;
+                        if (idx < list.get(mid))
+                            r = mid;
+                        else
+                            l = mid + 1;
+                    }
+                    if (idx >= list.get(r))
+                        continue out;
+                    idx = list.get(r);
+                }
+                cnt++;
+            }
+            return cnt;
+        }
+
+        public int numMatchingSubseq8(String s, String[] words) {
+            List<Integer>[] char2indices = new List[27];
+            for (int i = 0; i < 27; i++)
+                char2indices[i] = new ArrayList<>();
+
+            for (int i = 0; i < s.length(); i++)
+                char2indices[s.charAt(i) & 31].add(i);
+
+            int cnt = 0;
+            out:
+            for (String word : words) {
+                if (word.length() > s.length()) continue;
+                for (int i = 0, idx = -1; i < word.length(); i++) {
+                    List<Integer> list = char2indices[word.charAt(i) & 31];
+                    if (list.isEmpty() || list.get(list.size() - 1) <= idx)
+                        continue out;
+                    idx = binarySearch(list, idx);
+                }
+                cnt++;
+            }
+            return cnt;
+        }
+
+        public int numMatchingSubseq(String s, String[] words) {
+            int len = s.length();
+            int idx = 0;
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].length() <= len)
+                    words[idx++] = words[i];
+            }
+
+            int[] pointers = new int[idx];
+            int cnt = 0;
+            for (int i = 0; i < len; i++) {
+                char c = s.charAt(i);
+                for (int j = 0; j < idx; j++) {
+                    if (pointers[j] >= words[j].length())
+                        continue;
+                    if (c == words[j].charAt(pointers[j])) {
+                        if (++pointers[j] == words[j].length()) {
+                            cnt++;
+                        }
+                    }
+                }
+            }
+            return cnt;
+        }
+
+        private int binarySearch(List<Integer> list, int target) {
+            int l = 0, r = list.size() - 1;
+            while (l < r) {
+                int mid = l + r >> 1;
+                if (target < list.get(mid))
+                    r = mid;
+                else
+                    l = mid + 1;
+            }
+            return list.get(r);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+}
---
+++ _2_algorithm/slidingWindow_doublePointer/IsSubsequence.java
@@ -0,0 +1,76 @@
+//<p>给定字符串 <strong>s</strong> 和 <strong>t</strong> ，判断 <strong>s</strong> 是否为 <strong>t</strong> 的子序列。</p>
+//
+//<p>字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，<code>"ace"</code>是<code>"abcde"</code>的一个子序列，而<code>"aec"</code>不是）。</p>
+//
+//<p><strong>进阶：</strong></p>
+//
+//<p>如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k &gt;= 10亿，你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？</p>
+//
+//<p><strong>致谢：</strong></p>
+//
+//<p>特别感谢<strong> </strong><a href="https://leetcode.com/pbrother/">@pbrother&nbsp;</a>添加此问题并且创建所有测试用例。</p>
+//
+//<p>&nbsp;</p>
+//
+//<p><strong>示例 1：</strong></p>
+//
+//<pre>
+//<strong>输入：</strong>s = "abc", t = "ahbgdc"
+//<strong>输出：</strong>true
+//</pre>
+//
+//<p><strong>示例 2：</strong></p>
+//
+//<pre>
+//<strong>输入：</strong>s = "axc", t = "ahbgdc"
+//<strong>输出：</strong>false
+//</pre>
+//
+//<p>&nbsp;</p>
+//
+//<p><strong>提示：</strong></p>
+//
+//<ul> 
+// <li><code>0 &lt;= s.length &lt;= 100</code></li> 
+// <li><code>0 &lt;= t.length &lt;= 10^4</code></li> 
+// <li>两个字符串都只由小写字符组成。</li> 
+//</ul>
+//
+//<div><li>👍 752</li><li>👎 0</li></div>
+package org.example.leetcode.problems._2_algorithm.slidingWindow_doublePointer;
+
+//392.判断子序列
+//开题时间：2022-11-17 13:51:12
+public class IsSubsequence {
+    public static void main(String[] args) {
+        Solution solution = new IsSubsequence().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isSubsequence9(String s, String t) {
+            if (s.length() > t.length())
+                return false;
+
+            for (int i = 0, j = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                while (j < t.length() && c != t.charAt(j))
+                    j++;
+                if (j == t.length())
+                    return false;
+                j++;
+            }
+            return true;
+        }
+
+        public boolean isSubsequence(String s, String t) {
+            for (int i = 0, j = -1; i < s.length(); i++)
+                if ((j = t.indexOf(s.charAt(i), j + 1)) == -1)
+                    return false;
+            return true;
+        }
+
+        //todo DP
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+}
