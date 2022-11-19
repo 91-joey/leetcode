3b340740132b41270983b370ea42ebbf0b403f72
joeyLocal <joey.91@qq.com>
2022-11-19 09:06:35 +0800

    1732.找到最高海拔

---
+++ _2_algorithm/prefix_sum/FindTheHighestAltitude.java
@@ -0,0 +1,55 @@
+//<p>有一个自行车手打算进行一场公路骑行，这条路线总共由&nbsp;<code>n + 1</code>&nbsp;个不同海拔的点组成。自行车手从海拔为 <code>0</code>&nbsp;的点&nbsp;<code>0</code>&nbsp;开始骑行。</p>
+//
+//<p>给你一个长度为 <code>n</code>&nbsp;的整数数组&nbsp;<code>gain</code>&nbsp;，其中 <code>gain[i]</code>&nbsp;是点 <code>i</code>&nbsp;和点 <code>i + 1</code>&nbsp;的 <strong>净海拔高度差</strong>（<code>0 &lt;= i &lt; n</code>）。请你返回 <strong>最高点的海拔</strong> 。</p>
+//
+//<p>&nbsp;</p>
+//
+//<p><strong>示例 1：</strong></p>
+//
+//<pre>
+//<b>输入：</b>gain = [-5,1,5,0,-7]
+//<b>输出：</b>1
+//<b>解释：</b>海拔高度依次为 [0,-5,-4,1,1,-6] 。最高海拔为 1 。
+//</pre>
+//
+//<p><strong>示例 2：</strong></p>
+//
+//<pre>
+//<b>输入：</b>gain = [-4,-3,-2,-1,4,3,2]
+//<b>输出：</b>0
+//<b>解释：</b>海拔高度依次为 [0,-4,-7,-9,-10,-6,-3,-1] 。最高海拔为 0 。
+//</pre>
+//
+//<p>&nbsp;</p>
+//
+//<p><strong>提示：</strong></p>
+//
+//<ul> 
+// <li><code>n == gain.length</code></li> 
+// <li><code>1 &lt;= n &lt;= 100</code></li> 
+// <li><code>-100 &lt;= gain[i] &lt;= 100</code></li> 
+//</ul>
+//
+//<div><li>👍 30</li><li>👎 0</li></div>
+package org.example.leetcode.problems._2_algorithm.prefix_sum;
+
+//1732.找到最高海拔
+//开题时间：2022-11-19 08:58:30
+public class FindTheHighestAltitude {
+    public static void main(String[] args) {
+        Solution solution = new FindTheHighestAltitude().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int largestAltitude(int[] gain) {
+            int max = 0, cur = 0;
+            for (int e : gain) {
+                cur += e;
+                max = Math.max(max, cur);
+            }
+            return max;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+}
