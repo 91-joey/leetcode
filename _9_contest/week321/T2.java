1f704ad6c7edf65dfba1dc3e53324202569b1d2d
joeyLocal <joey.91@qq.com>
2022-11-27 18:17:12 +0800

    6246. Append Characters to String to Make Subsequence

--- _9_contest/week321/T2.java
+++ _9_contest/week321/T2.java
@@ -1,18 +1,19 @@
 package org.example.leetcode.problems._9_contest.week321;
 
+//6246. Append Characters to String to Make Subsequence
 public class T2 {
     public static void main(String[] args) {
-
+        System.out.println(appendCharacters("coaching", "coding"));
     }
 
-    public int appendCharacters(String s, String t) {
+    public int appendCharacters9(String s, String t) {
         char[] sChars = s.toCharArray();
         char[] tChars = t.toCharArray();
         int m = sChars.length;
         int n = tChars.length;
 
         int i = 0, j = 0;
-        for (; i < m && j < n; ) {
+        while (i < m && j < n) {
             if (sChars[i] == tChars[j])
                 j++;
             i++;
@@ -21,4 +22,12 @@ public class T2 {
         return n - j;
     }
 
+    //双指针
+    public static int appendCharacters(String s, String t) {
+        int j = 0, n = t.length();
+        for (int i = 0, m = s.length(); i < m && j < n; i++)
+            if (s.charAt(i) == t.charAt(j))
+                j++;
+        return n - j;
+    }
 }
