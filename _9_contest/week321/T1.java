e1da1d2dcbe038d71de5e3e9bd55e692c471deb0
joeyLocal <joey.91@qq.com>
2022-11-27 16:52:13 +0800

    6245. Find the Pivot Integer

--- _9_contest/week321/T1.java
+++ _9_contest/week321/T1.java
@@ -1,26 +1,52 @@
 package org.example.leetcode.problems._9_contest.week321;
 
+//6245. Find the Pivot Integer
 public class T1 {
     public static void main(String[] args) {
-
+        System.out.println(pivotInteger(8));
     }
 
-    public int pivotInteger(int n) {
+    //BL    n^2
+    public int pivotInteger9(int n) {
         for (int i = 1; i <= n; i++) {
-            int sum1 = 0;
-            for (int j = 1; j <= i; j++) {
-                sum1 += j;
-            }
+            int pre = 0;
+            for (int j = 1; j <= i; j++) pre += j;
 
-            int sum2 = 0;
-            for (int j = i; j <= n; j++) {
-                sum2 += j;
-            }
+            int suf = 0;
+            for (int j = i; j <= n; j++) suf += j;
 
-            if (sum1 == sum2)
+            if (pre == suf)
                 return i;
         }
         return -1;
     }
 
+    public static int pivotInteger8(int n) {
+        int sum = (1 + n) * n / 2;
+        for (int i = 1, pre = 0; i <= n; i++) {
+            pre += i;
+            if (pre == sum - pre + i)
+                return i;
+        }
+        return -1;
+    }
+
+    public static int pivotInteger7(int n) {
+        for (int l = 1, r = n, tmp = (n + 1) * n / 2; l <= r; ) {
+            int mid = ((r - l) >> 1) + l;
+            int diff = mid * mid - tmp;
+            if (diff == 0)
+                return mid;
+            else if (diff < 0)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return -1;
+    }
+
+    public static int pivotInteger(int n) {
+        double ans = Math.sqrt(n * (n + 1) / 2);
+        return ans == (int) ans ? (int) ans : -1;
+    }
 }
