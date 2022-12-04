4bde4b68a08472bb7719aa9af59af9ad6e36cdbf
joeyLocal <joey.91@qq.com>
2022-12-04 14:07:49 +0800

    6253. Circular Sentence

--- _9_contest/week322/T1.java
+++ _9_contest/week322/T1.java
@@ -1,15 +1,20 @@
 package org.example.leetcode.problems._9_contest.week322;
 
+//6253. Circular Sentence
 public class T1 {
-    public static void main(String[] args) {
-
-    }
-
-    public boolean isCircularSentence(String sentence) {
+    public boolean isCircularSentence9(String sentence) {
         String[] split = sentence.split(" ");
         for (int i = 0; i < split.length; i++)
             if (split[i].charAt(split[i].length() - 1) !=
                     split[(i + 1) % split.length].charAt(0))
+                return false;
+        return true;
+    }
+
+    public boolean isCircularSentence(String sentence) {
+        sentence += " ";
+        for (int i = 1; i < sentence.length(); i++)
+            if (sentence.charAt(i) == ' ' && sentence.charAt(i - 1) != sentence.charAt((i + 1) % sentence.length()))
                 return false;
         return true;
     }
