a4464276f94cc95ccb84bfc96b75135c199894a4
joeyLocal <joey.91@qq.com>
2022-12-06 11:48:59 +0800

    1672. 最富有客户的资产总量

--- _1_dataStructure/arrayAndString/MaximumWealth.java
+++ _1_dataStructure/arrayAndString/MaximumWealth.java
@@ -1,8 +1,10 @@
 package org.example.leetcode.problems._1_dataStructure.arrayAndString;
 
+import java.util.Arrays;
+
 //1672. 最富有客户的资产总量
 public class MaximumWealth {
-    public int maximumWealth(int[][] accounts) {
+    public int maximumWealth9(int[][] accounts) {
         int max=Integer.MIN_VALUE;
         for (int[] account : accounts) {
             int cur=0;
@@ -11,6 +13,13 @@ public class MaximumWealth {
             }
             max=Math.max(max,cur);
         }
+        return max;
+    }
+
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for (int[] account : accounts)
+            max = Math.max(max, Arrays.stream(account).sum());
         return max;
     }
 }
