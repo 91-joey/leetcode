761bb4bdfc16f7ec3bfc2f105ff7c45c961ca3bb
joeyLocal <joey.91@qq.com>
2022-12-11 15:01:35 +0800

    6258. 数组中最长的方波

--- _9_contest/week323/T2.java
+++ _9_contest/week323/T2.java
@@ -3,29 +3,36 @@ package org.example.leetcode.problems._9_contest.week323;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+//6258. 数组中最长的方波
 public class T2 {
     public static void main(String[] args) {
 
     }
 
-    public int longestSquareStreakX(int[] nums) {
+    //哈希表 + 动态规划
+    public int longestSquareStreak6(int[] nums) {
         Arrays.sort(nums);
+
+        HashMap<Integer, Integer> dp = new HashMap<>();
+        for (int num : nums)
+            dp.put(num, 1);
 
         int max = 1;
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++)
-                if (nums[j] * nums[j] == nums[i])
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-            max = Math.max(max, dp[i]);
+            double sqrt = Math.sqrt(nums[i]);
+            if ((int) sqrt == sqrt) {
+                dp.put(nums[i], dp.getOrDefault((int) sqrt, 0) + 1);
+                max = Math.max(max, dp.get(nums[i]));
+            }
         }
-        return max == 1 ? -1 : max;
+        return max <= 1 ? -1 : max;
     }
 
-    public int longestSquareStreak(int[] nums) {
+    public int longestSquareStreak9(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for (int num : nums)
             set.add(num);
@@ -33,11 +40,40 @@ public class T2 {
         int max = -1;
         for (int num : nums) {
             int cnt = 1;
-            for (int target = num * num; set.contains(target); target *= target) {
+            for (int target = num * num; set.contains(target); target *= target)
                 cnt++;
-            }
             max = Math.max(max, cnt);
         }
         return max == 1 ? -1 : max;
+    }
+
+    public int longestSquareStreak8(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums)
+            set.add(num);
+
+        int max = -1;
+        for (int num : set) {
+            int cnt = 1;
+            for (int target = num * num; set.contains(target); target *= target)
+                cnt++;
+            max = Math.max(max, cnt);
+        }
+        return max <= 1 ? -1 : max;
+    }
+
+    //☆☆☆☆☆ 哈希表 + 枚举
+    public int longestSquareStreak(int[] nums) {
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+
+        int max = -1;
+        for (int num : set) {
+            int cnt = 0;
+            for (; set.contains(num); num *= num)
+                cnt++;
+            max = Math.max(max, cnt);
+        }
+
+        return max <= 1 ? -1 : max;
     }
 }
