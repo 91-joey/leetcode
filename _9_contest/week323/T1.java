22f192a8d90b48db4eb55d15e5325fb9bebac7d9
joeyLocal <joey.91@qq.com>
2022-12-11 14:37:55 +0800

    6257. Delete Greatest Value in Each Row

--- _9_contest/week323/T1.java
+++ _9_contest/week323/T1.java
@@ -1,9 +1,8 @@
 package org.example.leetcode.problems._9_contest.week323;
 
-import org.example.leetcode.problems._3_common.tool.Tools;
+import java.util.Arrays;
 
-import java.util.ArrayList;
-
+//6257. Delete Greatest Value in Each Row
 public class T1 {
     public static void main(String[] args) {
         System.out.println(deleteGreatestValue(new int[][]{
@@ -13,20 +12,51 @@ public class T1 {
     }
 
 
-    public static int deleteGreatestValue(int[][] grid) {
+    //模拟    n*m*n
+    public static int deleteGreatestValue9(int[][] grid) {
         int ans = 0;
         for (int i = 0; i < grid[0].length; i++) {
             int max = 0;
             for (int[] arr : grid) {
                 int idx = 0;
-                for (int j = 1; j < arr.length; j++) {
+                for (int j = 1; j < arr.length; j++)
                     if (arr[j] > arr[idx])
                         idx = j;
-                }
                 max = Math.max(max, arr[idx]);
                 arr[idx] = Integer.MIN_VALUE;
             }
             ans += max;
+        }
+        return ans;
+    }
+
+    //排序    m*nlogn+m*n = m*nlogn
+    public static int deleteGreatestValue8(int[][] grid) {
+        for (int[] arr : grid)
+            Arrays.sort(arr);
+
+        int ans = 0;
+        for (int j = 0; j < grid[0].length; j++) {
+            int max = 0;
+            for (int i = 0; i < grid.length; i++)
+                max = Math.max(max, grid[i][j]);
+            ans += max;
+        }
+        return ans;
+    }
+
+    //☆☆☆☆☆ 排序（Stream求列最大值）
+    public static int deleteGreatestValue(int[][] grid) {
+        for (int[] arr : grid)
+            Arrays.sort(arr);
+
+        int ans = 0;
+        for (int j = 0; j < grid[0].length; j++) {
+            int finalJ = j;
+            ans += Arrays.stream(grid)
+                    .mapToInt(arr -> arr[finalJ])
+                    .max()
+                    .getAsInt();
         }
         return ans;
     }
