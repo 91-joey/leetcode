416419934c615a06a50df5ddad3bbef5c6725ca6
joeyLocal <joey.91@qq.com>
2022-12-14 18:28:34 +0800

    363.矩形区域不超过 K 的最大数值和

--- _2_algorithm/dp/MaxSumOfRectangleNoLargerThanK.java
+++ _2_algorithm/dp/MaxSumOfRectangleNoLargerThanK.java
@@ -110,7 +110,7 @@ public class MaxSumOfRectangleNoLargerThanK {
             return max;
         }
 
-        //滚动数组
+        //region 滚动数组
         public int maxSumSubmatrix8(int[][] matrix, int k) {
             int m = matrix.length;
             int n = matrix[0].length;
@@ -160,9 +160,10 @@ public class MaxSumOfRectangleNoLargerThanK {
             }
             return max;
         }
+        //endregion
 
-        //有序集合
-        public int maxSumSubmatrix(int[][] matrix, int k) {
+        //有序集合（官解）
+        public int maxSumSubmatrix6(int[][] matrix, int k) {
             int m = matrix.length;
             int n = matrix[0].length;
 
@@ -185,6 +186,111 @@ public class MaxSumOfRectangleNoLargerThanK {
                                 return k;
                         }
                         set.add(prefix);
+                    }
+                }
+            }
+
+            return max;
+        }
+
+        //二维前缀和
+        public int maxSumSubmatrix5(int[][] matrix, int k) {
+            int m = matrix.length + 1;
+            int n = matrix[0].length + 1;
+
+            int[][] prefix = new int[m][n];
+            for (int i = 1; i < m; i++)
+                for (int j = 1; j < n; j++)
+                    prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + matrix[i - 1][j - 1];
+
+            int max = Integer.MIN_VALUE;
+            for (int i = 1; i < m; i++)
+                for (int j = 1; j < n; j++)
+                    for (int p = i; p < m; p++)
+                        for (int q = j; q < n; q++) {
+                            int sum = prefix[p][q] + prefix[i - 1][j - 1] - prefix[i - 1][q] - prefix[p][j - 1];
+                            if (sum == k)
+                                return k;
+                            else if (sum < k)
+                                max = Math.max(max, sum);
+                        }
+
+            return max;
+        }
+
+        //二维前缀和 + 有序集合
+        public int maxSumSubmatrix4(int[][] matrix, int k) {
+            int m = matrix.length + 1;
+            int n = matrix[0].length + 1;
+
+            if (m > n) {
+                int a = n - 1;
+                int b = m - 1;
+                int[][] transformed = new int[a][b];
+                for (int i = 0; i < a; i++)
+                    for (int j = 0; j < b; j++)
+                        transformed[i][j] = matrix[j][i];
+                return maxSumSubmatrix(transformed, k);
+            }
+
+            int[][] prefix = new int[m][n];
+            for (int i = 1; i < m; i++)
+                for (int j = 1; j < n; j++)
+                    prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + matrix[i - 1][j - 1];
+
+            int max = Integer.MIN_VALUE;
+            for (int top = 1; top < m; top++) {
+                for (int bot = top; bot < m; bot++) {
+                    TreeSet<Integer> set = new TreeSet<>();
+                    set.add(0);
+                    for (int j = 1; j < n; j++) {
+                        int right = prefix[bot][j] - prefix[top - 1][j];
+                        Integer left = set.ceiling(right - k);
+                        if (left != null)
+                            if (left == k)
+                                return k;
+                            else
+                                max = Math.max(max, right - left);
+                        set.add(right);
+                    }
+                }
+            }
+
+            return max;
+        }
+
+        //☆☆☆☆☆ 二维前缀和 + 有序集合 + 空间优化（前缀和的逻辑下放到搜索子矩阵的循环里去做）
+        public int maxSumSubmatrix(int[][] matrix, int k) {
+            int m = matrix.length + 1;
+            int n = matrix[0].length + 1;
+
+            if (m > n) {
+                int a = n - 1;
+                int b = m - 1;
+                int[][] transformed = new int[a][b];
+                for (int i = 0; i < a; i++)
+                    for (int j = 0; j < b; j++)
+                        transformed[i][j] = matrix[j][i];
+                return maxSumSubmatrix(transformed, k);
+            }
+
+            int max = Integer.MIN_VALUE;
+            for (int top = 1; top < m; top++) {
+                int[] prefix = new int[n];
+                for (int bot = top; bot < m; bot++) {
+                    TreeSet<Integer> set = new TreeSet<>();
+                    set.add(0);
+                    int right = 0;
+                    for (int j = 1; j < n; j++) {
+                        prefix[j] += matrix[bot - 1][j - 1];
+                        right += prefix[j];
+                        Integer left = set.ceiling(right - k);
+                        if (left != null)
+                            if (left == k)
+                                return k;
+                            else
+                                max = Math.max(max, right - left);
+                        set.add(right);
                     }
                 }
             }
