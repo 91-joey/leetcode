3090a0d9548fdc1f5c54e7b3c8e0045453c0dceb
joeyLocal <joey.91@qq.com>
2022-12-18 18:29:58 +0800

    6266. Smallest Value After Replacing With Sum of Prime Factors

--- _9_contest/week324/T1.java
+++ _9_contest/week324/T1.java
@@ -2,23 +2,40 @@ package org.example.leetcode.problems._9_contest.week324;
 
 import java.util.HashMap;
 
+//6265. Count Pairs Of Similar Strings
 public class T1 {
     public static void main(String[] args) {
         System.out.println(similarPairs(new String[]{"aba", "aabb", "abcd", "bac", "aabc"}));
     }
 
-    public static int similarPairs(String[] words) {
+    //哈希计数 + 状态压缩（位运算）+ 依次统计：
+    public static int similarPairs9(String[] words) {
         int ans = 0;
         HashMap<Integer, Integer> state2cnt = new HashMap<>();
         for (String word : words) {
             int state = 0;
-            for (int i = 0; i < word.length(); i++) {
-                char c = word.charAt(i);
-                state |= (1 << (c - 'a'));
-            }
+            for (int i = 0; i < word.length(); i++)
+                state |= (1 << (word.charAt(i) - 'a'));
             ans += state2cnt.getOrDefault(state, 0);
             state2cnt.merge(state, 1, Integer::sum);
         }
+        return ans;
+    }
+
+    //哈希计数 + 状态压缩（位运算）+ 最后统计：
+    public static int similarPairs(String[] words) {
+        HashMap<Integer, Integer> state2cnt = new HashMap<>();
+        for (String word : words) {
+            int state = 0;
+            for (int i = 0; i < word.length(); i++)
+                state |= (1 << (word.charAt(i) - 'a'));
+            state2cnt.merge(state, 1, Integer::sum);
+        }
+
+        int ans = 0;
+        for (int cnt : state2cnt.values())
+            ans += cnt * (cnt - 1) / 2;
+
         return ans;
     }
 }
--- _9_contest/week324/T2.java
+++ _9_contest/week324/T2.java
@@ -1,10 +1,14 @@
 package org.example.leetcode.problems._9_contest.week324;
 
+//6266. Smallest Value After Replacing With Sum of Prime Factors
 public class T2 {
     public static void main(String[] args) {
 //        System.out.println(smallestValue(36));
         System.out.println(smallestValue(4));
 //        System.out.println(smallestValue(15));
+        for (int i = 2; i < 1000; i++) {
+            System.out.println(i+" : " + smallestValue(i));
+        }
     }
 
     public static int smallestValueX(int n) {
@@ -21,7 +25,7 @@ public class T2 {
         return 0;
     }
 
-    public static int smallestValue(int n) {
+    public static int smallestValue9(int n) {
         int pre = 0;
         int sum = -1;
         while (pre != sum) {
@@ -38,6 +42,24 @@ public class T2 {
             n = sum;
         }
         return sum;
+    }
+
+    //☆☆☆☆☆ 质因数分解，质因数和与原始值相同即返回 $O(\sqrt n)$
+    public static int smallestValue(int n) {
+        while (true) {
+            int sum = 0;
+            int x = n;
+            for (int i = 2; i * i <= x; i++)
+                while (x % i == 0) {
+                    sum += i;
+                    x /= i;
+                }
+            if (x > 1)
+                sum += x;
+            if (sum == n)
+                return n;
+            n = sum;
+        }
     }
 
     private static int getSum(int n) {
