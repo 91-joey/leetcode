3e8896a61b4f0741c17ab8af4cfc4fe176adc35a
joeyLocal <joey.91@qq.com>
2022-12-25 12:00:29 +0800

    week325

--- _9_contest/week325/T1.java
+++ _9_contest/week325/T1.java
@@ -5,5 +5,14 @@ public class T1 {
 
     }
 
-    
+    public int closetTarget(String[] words, String target, int startIndex) {
+        int n = words.length;
+        for (int i = 0; i <= (n + 1) / 2; i++) {
+            if (target.equals(words[(startIndex - i + n) % n]) ||
+                    target.equals(words[(startIndex + i) % n]))
+                return i;
+        }
+        return -1;
+    }
+
 }
--- _9_contest/week325/T2.java
+++ _9_contest/week325/T2.java
@@ -2,8 +2,39 @@ package org.example.leetcode.problems._9_contest.week325;
 
 public class T2 {
     public static void main(String[] args) {
-
+        System.out.println(takeCharacters("aabaaaacaabc", 2));
+        System.out.println(takeCharacters("a", 0));
     }
 
-    
+    public static int takeCharacters(String s, int k) {
+        char[] cs = s.toCharArray();
+        int[] freq = new int[3];
+        for (char c : cs) {
+            freq[c - 'a']++;
+        }
+
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] < k)
+                return -1;
+            freq[i] -= k;
+        }
+
+        int n = cs.length;
+
+        int l = 0, r = 0;
+        int max = 0;
+        while (r < n) {
+            if (--freq[cs[r] - 'a'] < 0) {
+                max = Math.max(max, r - l);
+                while (freq[cs[r] - 'a'] < 0)
+                    ++freq[cs[l++] - 'a'];
+            }
+            r++;
+        }
+        max = Math.max(max, r - l);
+
+        return n - max;
+//        return n - (r - l);
+    }
+
 }
