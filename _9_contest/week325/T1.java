19d09a201c635db9b854c62b24db15fff20819b3
joeyLocal <joey.91@qq.com>
2022-12-25 14:59:27 +0800

    6269. Shortest Distance to Target String in a Circular Array

--- _9_contest/week325/T1.java
+++ _9_contest/week325/T1.java
@@ -1,17 +1,18 @@
 package org.example.leetcode.problems._9_contest.week325;
 
+//6269. Shortest Distance to Target String in a Circular Array
 public class T1 {
     public static void main(String[] args) {
 
     }
 
+    //中心扩散
     public int closetTarget(String[] words, String target, int startIndex) {
         int n = words.length;
-        for (int i = 0; i <= (n + 1) / 2; i++) {
+        for (int i = 0; i <= n / 2; i++)
             if (target.equals(words[(startIndex - i + n) % n]) ||
                     target.equals(words[(startIndex + i) % n]))
                 return i;
-        }
         return -1;
     }
 
