57bb5a1ec41cfb11133d48322d71b031fae06afc
joeyLocal <joey.91@qq.com>
2023-01-01 12:24:49 +0800

    6279. Distinct Prime Factors of Product of Array

--- _9_contest/week326/T2.java
+++ _9_contest/week326/T2.java
@@ -2,12 +2,13 @@ package org.example.leetcode.problems._9_contest.week326;
 
 import java.util.HashSet;
 
+//6279. Distinct Prime Factors of Product of Array
 public class T2 {
     public static void main(String[] args) {
 
     }
 
-
+    //分解质因数
     public int distinctPrimeFactors(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for (int x : nums) {
