b7af1307ecebd2a1ea6926915cab4f5cb9b57894
joeyLocal <joey.91@qq.com>
2023-01-05 14:01:27 +0800

    542.01 矩阵

--- _1_dataStructure/queueAndStack/Zero1Matrix.java
+++ _1_dataStructure/queueAndStack/Zero1Matrix.java
@@ -38,8 +38,11 @@
 //<div><div>Related Topics</div><div><li>广度优先搜索</li><li>数组</li><li>动态规划</li><li>矩阵</li></div></div><br><div><li>👍 745</li><li>👎 0</li></div>
 package org.example.leetcode.problems._1_dataStructure.queueAndStack;
 
+import org.example.leetcode.problems._3_common.tool.Tools;
+
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
 
 //542.01 矩阵
@@ -47,11 +50,11 @@ import java.util.Queue;
 public class Zero1Matrix {
     public static void main(String[] args) {
         Solution solution = new Zero1Matrix().new Solution();
-        System.out.println(Arrays.deepToString(solution.updateMatrix(new int[][]{
-                {0, 0, 0},
-                {0, 1, 0},
-                {1, 1, 1}
-        })));
+//        System.out.println(Arrays.deepToString(solution.updateMatrix(Tools.to2DIntArray("[[0,1,0],[0,1,0],[0,1,0],[0,1,0],[0,1,0]]"))));
+        System.out.println(Arrays.deepToString(solution.updateMatrix(Tools.to2DIntArray("[[0,1,0,1,1],[1,1,0,0,1],[0,0,0,1,0],[1,0,1,1,1],[1,0,0,0,1]]"))));
+//        System.out.println(Arrays.deepToString(solution.updateMatrix(Tools.to2DIntArray("[[0],[0],[0],[0],[0]]"))));
+//        System.out.println(Arrays.deepToString(solution.updateMatrix(Tools.to2DIntArray("[[0,0,0],[0,1,0],[1,1,1]]"))));
+        System.out.println("[[2,1,0,0,1,0,0,1,1,0],[1,0,0,1,0,1,1,2,2,1],[1,1,1,0,0,1,2,2,1,0],[0,1,2,1,0,1,2,3,2,1],[0,0,1,2,1,2,1,2,1,0],[1,1,2,3,2,1,0,1,1,1],[0,1,2,3,2,1,1,0,0,1],[1,2,1,2,1,0,0,1,1,2],[0,1,0,1,1,0,1,2,2,3],[1,2,1,0,1,0,1,2,3,4]]");
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -65,7 +68,7 @@ public class Zero1Matrix {
         public static final int ZERO = 0;
 
         //1.BFS+队列
-        public int[][] updateMatrix(int[][] mat) {
+        public int[][] updateMatrix88(int[][] mat) {
             int m = mat.length;
             int n = mat[0].length;
             Queue<int[]> queue = new ArrayDeque<>();
@@ -100,6 +103,68 @@ public class Zero1Matrix {
             }
 
             return mat;
+        }
+
+        public static final int[] DIRS = {1, 0, -1, 0, 1};
+
+        //☆☆☆☆ 多源BFS
+        public int[][] updateMatrix9(int[][] mat) {
+            int m = mat.length;
+            int n = mat[0].length;
+            Queue<int[]> q = new LinkedList<>();
+            boolean[][] vis = new boolean[m][n];
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                    if (mat[i][j] == 0) {
+                        q.offer(new int[]{i, j});
+                        vis[i][j] = true;
+                    }
+
+            while (!q.isEmpty()) {
+                int[] poll = q.poll();
+                int r = poll[0];
+                int c = poll[1];
+                for (int i = 0; i < 4; i++) {
+                    int rNew = r + DIRS[i];
+                    int cNew = c + DIRS[i + 1];
+                    if (0 <= rNew && rNew < m && 0 <= cNew && cNew < n &&
+                            !vis[rNew][cNew]) {
+                        mat[rNew][cNew] = mat[r][c] + 1;
+                        q.offer(new int[]{rNew, cNew});
+                        vis[rNew][cNew] = true;
+                    }
+                }
+            }
+
+            return mat;
+        }
+
+        public static final int INF = 0x3f3f3f3f;
+
+        //☆☆☆☆☆ 两次DP
+        public int[][] updateMatrix(int[][] mat) {
+            int m = mat.length;
+            int n = mat[0].length;
+            int[][] f = new int[m + 2][n + 2];
+            for (int i = 0; i < m + 2; i++) f[i][0] = f[i][n + 1] = INF;
+            for (int j = 0; j < n + 2; j++) f[0][j] = f[m + 1][j] = INF;
+
+            for (int i = 1; i < m + 1; i++)
+                for (int j = 1; j < n + 1; j++)
+                    if (mat[i - 1][j - 1] != 0)
+                        f[i][j] = Math.min(f[i - 1][j], f[i][j - 1]) + 1;
+
+            for (int i = m; i > 0; i--)
+                for (int j = n; j > 0; j--)
+                    if (mat[i - 1][j - 1] != 0)
+                        f[i][j] = Math.min(f[i][j], Math.min(f[i + 1][j], f[i][j + 1]) + 1);
+
+            int[][] ans = new int[m][];
+            for (int i = 0; i < m; i++)
+                ans[i] = Arrays.copyOfRange(f[i + 1], 1, n + 1);
+
+            return ans;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
