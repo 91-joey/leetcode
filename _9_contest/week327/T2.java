d1928faff3645f47a0621b9f8a1fca399328be20
joeyLocal <joey.91@qq.com>
2023-01-08 14:08:42 +0800

    6285. Maximal Score After Applying K Operations

--- _9_contest/week327/T2.java
+++ _9_contest/week327/T2.java
@@ -3,11 +3,13 @@ package org.example.leetcode.problems._9_contest.week327;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
+//6285. Maximal Score After Applying K Operations
 public class T2 {
     public static void main(String[] args) {
 
     }
 
+    //优先队列
     public long maxKelements(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         for (int x : nums)
@@ -15,9 +17,10 @@ public class T2 {
 
         long ans = 0;
         for (int i = 0; i < k; i++) {
-            Integer poll = pq.poll();
+            int poll = pq.poll();
             ans += poll;
-            pq.offer((int) Math.ceil((double) poll / 3));
+            pq.offer((poll + 2) / 3);
+//            pq.offer((int) Math.ceil((double) poll / 3));
         }
 
         return ans;
