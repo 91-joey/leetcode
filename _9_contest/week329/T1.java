a3e6182942692360ea0a580507de57a2eca20595
joeyLocal <joey.91@qq.com>
2023-01-15 15:18:46 +0800

    6292. Increment Submatrices by One

---
+++ _9_contest/week329/T1.java
@@ -0,0 +1,13 @@
+package org.example.leetcode.problems._9_contest.week329;
+
+//
+public class T1 {
+    public static void main(String[] args) {
+        Solution solution = new T1().new Solution();
+        System.out.println(solution);
+    }
+
+    class Solution {
+
+    }
+}
---
+++ _9_contest/week329/T3.java
@@ -0,0 +1,13 @@
+package org.example.leetcode.problems._9_contest.week329;
+
+//
+public class T3 {
+    public static void main(String[] args) {
+        Solution solution = new T3().new Solution();
+        System.out.println(solution);
+    }
+
+    class Solution {
+
+    }
+}
