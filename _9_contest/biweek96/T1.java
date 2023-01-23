475b19cb451132874f9f65d1a0b52040eefdefa2
joeyLocal <joey.91@qq.com>
2023-01-23 15:56:14 +0800

    2540. Minimum Common Value

--- _9_contest/biweek96/T1.java
+++ _9_contest/biweek96/T1.java
@@ -1,6 +1,10 @@
 package org.example.leetcode.problems._9_contest.biweek96;
 
-//
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+//2540. Minimum Common Value
 public class T1 {
     public static void main(String[] args) {
         Solution solution = new T1().new Solution();
@@ -8,7 +12,8 @@ public class T1 {
     }
 
     class Solution {
-        public int getCommon(int[] nums1, int[] nums2) {
+        //☆☆☆☆☆ 双指针
+        public int getCommon9(int[] nums1, int[] nums2) {
             int i = 0, j = 0;
             while (i < nums1.length && j < nums2.length) {
                 if (nums1[i] == nums2[j]) {
@@ -20,6 +25,12 @@ public class T1 {
                 }
             }
             return -1;
+        }
+
+        //哈希表
+        public int getCommon(int[] nums1, int[] nums2) {
+            Set<Integer> set = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+            return Arrays.stream(nums2).filter(set::contains).findFirst().orElse(-1);
         }
     }
 }
