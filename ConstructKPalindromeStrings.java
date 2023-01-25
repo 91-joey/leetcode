05fd9fd0eedd138e2ff2b9e63d6aba12cf751c41
joeyLocal <joey.91@qq.com>
2023-01-25 13:58:06 +0800

    1400.构造 K 个回文字符串

--- ConstructKPalindromeStrings.java
+++ ConstructKPalindromeStrings.java
@@ -76,7 +76,7 @@ public class ConstructKPalindromeStrings {
             int[] freq = new int[26];
             for (int i = 0; i < s.length(); i++) {
                 int idx = s.charAt(i) - 'a';
-                freq[idx] ^= 1;
+                freq[idx] ^= 1;//利用 异或位运算 进行 0-1 交替变换
             }
             return Arrays.stream(freq).sum() <= k;
         }
@@ -84,7 +84,16 @@ public class ConstructKPalindromeStrings {
         /*
          * ☆☆☆☆☆ 贪心 + 状态压缩
          * 首先字符串长度 >= 1，所以， len(s) 一定要 >= k
+         * 其次，回文字符串的长度分奇偶性，除了奇数长度的回文字符串中间的一个字符没有成对、其余字符都是成对存在。
          *
+         * 我们对字符串 s 中的字符进行计数：
+         *  - 若字符频率为偶数，则可以任意分配字符对（2个相同字符），去新建一个回文字符串、或者加在已有回文字符串的两侧（插入亦可，只要对称）
+         *  - 若字符频率为奇数，除去能够分配的字符对，还剩余一个无法成对的字符，这个字符只能成为奇数长度回文字符串的中心
+         *
+         * 总结，频率为奇数的字符，只能一人带一个「奇数长度回文字符串」（设一共 x 个），则剩余的 k - x 个为「偶数长度回文字符串」
+         *  k - x >= 0，即 x <= k
+         *
+         * 由于字符限定为小写英文字母，一共 26 个，故可以进行状态压缩
          */
         public boolean canConstruct8(String s, int k) {
             if (s.length() < k)
@@ -95,6 +104,7 @@ public class ConstructKPalindromeStrings {
             return Integer.bitCount(freq) <= k;
         }
 
+        //流炫技
         public boolean canConstruct(String s, int k) {
             if (s.length() < k)
                 return false;
