747807a34d3c79a2d3433b0dbd615221c3c711ce
joeyLocal <joey.91@qq.com>
2023-01-26 22:11:02 +0800

    280.摆动排序

--- WiggleSort.java
+++ WiggleSort.java
@@ -1,66 +1,79 @@
-//<p>给你一个的整数数组&nbsp;<code>nums</code>, 将该数组重新排序后使&nbsp;<code>nums[0] &lt;= nums[1] &gt;= nums[2] &lt;= nums[3]...</code>&nbsp;</p>
-//
-//<p>输入数组总是有一个有效的答案。</p>
-//
-//<p>&nbsp;</p>
-//
-//<p><strong>示例 1:</strong></p>
-//
-//<pre>
-//<strong>输入：</strong><span><code>nums = [3,5,2,1,6,4]</code></span>
-//<strong>输出：</strong>[3,5,1,6,2,4]
-//<strong>解释：</strong>[1,6,2,5,3,4]也是有效的答案</pre>
-//
-//<p><strong>示例 2:</strong></p>
-//
-//<pre>
-//<b>输入：</b>nums = [6,6,5,6,3,8]
-//<b>输出：</b>[6,6,5,6,3,8]
-//</pre>
-//
-//<p>&nbsp;</p>
-//
-//<p><strong>提示：</strong></p>
-//
-//<p>
-// <meta charset="UTF-8" /></p>
-//
-//<ul> 
-// <li><code>1 &lt;= nums.length &lt;= 5 * 10<sup>4</sup></code></li> 
-// <li><code>0 &lt;= nums[i] &lt;= 10<sup>4</sup></code></li> 
-// <li> <p>输入的&nbsp;<code>nums</code> 保证至少有一个答案。</p> </li> 
-//</ul>
-//
-//<p>&nbsp;</p>
-//
-//<p><b>进阶：</b>你能在&nbsp;<code>O(n)</code>&nbsp;时间复杂度下解决这个问题吗？</p>
-//
-//<div><li>👍 104</li><li>👎 0</li></div>
 package org.example.leetcode.problems;
 
 import java.util.Arrays;
 
-//280.摆动排序
-//开题时间：2023-01-26 18:26:15
+/**
+ * 280.摆动排序 <br>
+ * 开题时间：2023-01-26 18:26:15
+ */
 public class WiggleSort {
-    public static void main(String[] args) {
-        Solution solution = new WiggleSort().new Solution();
-        solution.wiggleSort(new int[]{3, 5, 2, 1, 6, 4});
+  public static void main(String[] args) {
+    Solution solution = new WiggleSort().new Solution();
+    solution.wiggleSort(new int[]{3, 5, 2, 1, 6, 4});
+  }
+  
+  //leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+    //排序 + 贪心 + 偶数索引取小数、奇数索引取大数
+    public void wiggleSort9(int[] nums) {
+      Arrays.sort(nums);
+      
+      int[] ans = new int[nums.length];
+      for (int i = 0; i * 2 < nums.length; i++) {
+        ans[i * 2] = nums[i];
+      }
+      for (int i = nums.length - 1 - (nums.length & 1), j = nums.length - 1; i >= 0; i -= 2, j--) {
+        ans[i] = nums[j];
+      }
+      
+      System.arraycopy(ans, 0, nums, 0, nums.length);
     }
-
-    //leetcode submit region begin(Prohibit modification and deletion)
-    class Solution {
-        public void wiggleSort(int[] nums) {
-            Arrays.sort(nums);
-            int[] ans = new int[nums.length];
-            for (int i = 0; i * 2 < nums.length; i++) {
-                ans[i * 2] = nums[i];
-            }
-            for (int i = nums.length - 1 - (nums.length & 1), j = nums.length - 1; i >= 0; i -= 2, j--) {
-                ans[i] = nums[j];
-            }
-            System.arraycopy(ans, 0, nums, 0, nums.length);
+    
+    //（简化）排序 + 贪心 + 偶数索引取小数、奇数索引取大数
+    public void wiggleSort8(int[] nums) {
+      Arrays.sort(nums);
+      
+      int n = nums.length;
+      int[] ans = new int[n];
+      for (int i = 0, l = 0, r = n - 1; i < n; i += 2, l++, r--) {
+        ans[i] = nums[l];
+        if (i + 1 < n)
+          ans[i + 1] = nums[r];
+      }
+      
+      System.arraycopy(ans, 0, nums, 0, nums.length);
+    }
+    
+    //（优化）排序 + 从索引 2 开始逐对交换元素
+    public void wiggleSort7(int[] nums) {
+      Arrays.sort(nums);
+      
+      for (int i = 2; i < nums.length; i += 2) {
+        swap(nums, i - 1, i);
+      }
+    }
+    
+    /*
+     * ☆☆☆☆☆ 一遍交换
+     * 用数学归纳法证明：
+     * 假设 [0,1, ..., k] 均已满足摆动排序，而 k+1 不满足，记此时 nums[k-1]，nums[k]，nums[k+1] 分别为 a，b，c
+     *  若不满足的是降序，则可知：c>b，同时 b>=a，此时三个数状态是 a <= b < c，交换 b 和 c 后变成：a < c > b，满足小大小
+     *  若不满足的是升序，则可知：c<b，同时 b<=a，此时三个数状态是 a >= b > c，交换 b 和 c 后变成：a > c < b，满足大小大
+     */
+    public void wiggleSort(int[] nums) {
+      for (int i = 1; i < nums.length; i++) {
+        if ((i % 2 == 0) == nums[i - 1] < nums[i]) {
+          swap(nums, i - 1, i);
         }
+      }
     }
-//leetcode submit region end(Prohibit modification and deletion)
+    
+    public static void swap(int[] arr, int i, int j) {
+      int tmp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = tmp;
+    }
+    
+  }
+  //leetcode submit region end(Prohibit modification and deletion)
 }
