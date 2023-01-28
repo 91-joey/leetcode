0c21038289e5eba19e37c0921b7588acc674c2c5
joeyLocal <joey.91@qq.com>
2023-01-28 14:34:58 +0800

    135.分发糖果

--- Candy.java
+++ Candy.java
@@ -47,7 +47,11 @@ public class Candy {
     }
     
     /*
-     * 贪心
+     * ☆☆☆☆☆ 贪心
+     * 我们可以将「相邻的孩子中，评分高的孩子必须获得更多的糖果」这句话拆分为两个规则，分别处理。
+     *  左规则：当 ratings[i−1]<ratings[i] 时，i 号学生的糖果数量将比 i−1 号孩子的糖果数量多。
+     *  右规则：当 ratings[i]>ratings[i+1] 时，i 号学生的糖果数量将比 i+1 号孩子的糖果数量多。
+     * 我们遍历该数组两次，处理出每一个学生分别满足左规则或右规则时，最少需要被分得的糖果数量。每个人最终分得的糖果数量即为这两个数量的最大值。
      */
     public int candy9(int[] ratings) {
       int n = ratings.length;
@@ -74,6 +78,7 @@ public class Candy {
       return ans;
     }
     
+    //贪心（精简版）
     public int candy(int[] ratings) {
       int n = ratings.length;
       int[] l2r = new int[n];
