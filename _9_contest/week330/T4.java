2b840b707c35c6e9cc8ca8ea9af010929ec132b4
joeyLocal <joey.91@qq.com>
2023-01-30 18:36:38 +0800

    2552. Count Increasing Quadruplets

--- _9_contest/week330/T4.java
+++ _9_contest/week330/T4.java
@@ -1,6 +1,6 @@
 package org.example.leetcode.problems._9_contest.week330;
 
-//
+// 2552. Count Increasing Quadruplets
 public class T4 {
   public static void main(String[] args) {
     Solution solution = new T4().new Solution();
@@ -8,7 +8,8 @@ public class T4 {
   }
   
   class Solution {
-    public long countQuadruplets(int[] nums) {
+    // brute force  n^4
+    public long countQuadrupletsTLE(int[] nums) {
       long ans = 0;
       int n = nums.length;
       for (int i = 0; i < n; i++) {
@@ -28,6 +29,62 @@ public class T4 {
           }
         }
       }
+      return ans;
+    }
+    
+    /*
+     * 有技巧的枚举 + 预处理前后缀 + 贡献法  n^2
+     * 预处理前后缀：
+     *  在 k 右侧的比 nums[j] 大的元素个数，记作 great[k][nums[j]]
+     *  在 j 左侧的比 nums[k] 小的元素个数，记作 less[j][nums[k]]
+     * 枚举 j 和 k，对于固定的 j 和 k，根据乘法原理，对答案的贡献为 less[j][nums[k]] * great[k][nums[j]]
+     */
+    public long countQuadruplets9(int[] nums) {
+      int n = nums.length;
+      
+      int[][] great = new int[n][n + 1]; // 在 k 右边、比 nums[j] 大的 l 的个数
+      for (int k = n - 2; k >= 2; k--) {
+        for (int x = 1; x < n + 1; x++) {
+          great[k][x] = great[k + 1][x] + (x < nums[k + 1] ? 1 : 0);
+        }
+      }
+      
+      long ans = 0;
+      int[] less = new int[n + 1]; // 在 j 左边、比 nums[k] 小的 i 的个数
+      for (int j = 1; j < n - 2; j++) {
+        for (int x = 1; x < n + 1; x++) {
+          less[x] += (nums[j - 1] < x ? 1 : 0);
+        }
+        for (int k = j + 1; k < n - 1; k++) {
+          if (nums[j] > nums[k]) {
+            ans += (long) less[nums[k]] * great[k][nums[j]];
+          }
+        }
+      }
+      
+      return ans;
+    }
+    
+    // 利用排列特性优化掉 less 数组
+    public long countQuadruplets(int[] nums) {
+      int n = nums.length;
+      
+      int[][] great = new int[n][n + 1]; // 在 k 右边、比 nums[j] 大的 l 的个数
+      for (int k = n - 2; k >= 1; k--) {
+        for (int x = 1; x < n + 1; x++) {
+          great[k][x] = great[k + 1][x] + (x < nums[k + 1] ? 1 : 0);
+        }
+      }
+      
+      long ans = 0;
+      for (int j = 1; j < n - 2; j++) {
+        for (int k = j + 1; k < n - 1; k++) {
+          if (nums[j] > nums[k]) {
+            ans += (long) (nums[k] - (n - j - 1 - great[j][nums[k]])) * great[k][nums[j]];
+          }
+        }
+      }
+      
       return ans;
     }
   }
