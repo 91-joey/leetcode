9b5c0e19c3e5d1a89c7c3f970784e71160a6f9e2
joeyLocal <joey.91@qq.com>
2023-01-31 17:11:46 +0800

    112.路径总和

--- _1_dataStructure/tree/PathSum.java
+++ _1_dataStructure/tree/PathSum.java
@@ -20,7 +20,7 @@
 //<strong>解释：</strong>树中存在两条根节点到叶子节点的路径：
 //(1 --&gt; 2): 和为 3
 //(1 --&gt; 3): 和为 4
-//不存在 sum = 5 的根节点到叶子节点的路径。</pre>
+// 不存在 sum = 5 的根节点到叶子节点的路径。</pre>
 //
 //<p><strong>示例 3：</strong></p>
 //
@@ -45,78 +45,80 @@ package org.example.leetcode.problems._1_dataStructure.tree;
 
 import org.example.leetcode.problems._3_common.tree.TreeNode;
 
-import java.util.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 
-//112.路径总和
-//开题时间：2022-09-14 18:05:02
+// 112.路径总和
+// 开题时间：2022-09-14 18:05:02
 public class PathSum {
-    public static void main(String[] args) {
-        Solution solution = new PathSum().new Solution();
+  public static void main(String[] args) {
+    Solution solution = new PathSum().new Solution();
+  }
+  
+  // leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+    // DFS+递归
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+      if (root == null)
+        return false;
+      if (root.left == null && root.right == null)
+        return root.val == targetSum;
+      return hasPathSum(root.left, targetSum - root.val) ||
+          hasPathSum(root.right, targetSum - root.val);
     }
-
-    //leetcode submit region begin(Prohibit modification and deletion)
-    class Solution {
-        //DFS+递归
-        public boolean hasPathSum(TreeNode root, int targetSum) {
-            if (root == null)
-                return false;
-            if (root.left == null && root.right == null)
-                return root.val == targetSum;
-            return hasPathSum(root.left, targetSum - root.val) ||
-                    hasPathSum(root.right, targetSum - root.val);
+    
+    // DFS+栈
+    public boolean hasPathSum_stack(TreeNode root, int targetSum) {
+      if (root == null) return false;
+      Deque<TreeNode> stack = new LinkedList<>();
+      stack.push(root);
+      
+      while (!stack.isEmpty()) {
+        TreeNode pop = stack.pop();
+        if (pop.left == null && pop.right == null) {
+          if (pop.val == targetSum)
+            return true;
+        } else {
+          if (pop.right != null) {
+            pop.right.val += pop.val;
+            stack.push(pop.right);
+          }
+          if (pop.left != null) {
+            pop.left.val += pop.val;
+            stack.push(pop.left);
+          }
         }
-
-        //DFS+栈
-        public boolean hasPathSum_stack(TreeNode root, int targetSum) {
-            if (root == null) return false;
-            Deque<TreeNode> stack = new LinkedList<>();
-            stack.push(root);
-
-            while (!stack.isEmpty()) {
-                TreeNode pop = stack.pop();
-                if (pop.left == null && pop.right == null) {
-                    if (pop.val == targetSum)
-                        return true;
-                } else {
-                    if (pop.right != null) {
-                        pop.right.val += pop.val;
-                        stack.push(pop.right);
-                    }
-                    if (pop.left != null) {
-                        pop.left.val += pop.val;
-                        stack.push(pop.left);
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        //BFS+queue
-        public boolean hasPathSum_BFS(TreeNode root, int targetSum) {
-            if (root == null) return false;
-            Queue<TreeNode> queue = new LinkedList<>();
-            queue.offer(root);
-
-            while (!queue.isEmpty()) {
-                TreeNode poll = queue.poll();
-                if (poll.left == null && poll.right == null) {
-                    if (poll.val == targetSum)
-                        return true;
-                } else {
-                    if (poll.left != null) {
-                        poll.left.val += poll.val;
-                        queue.offer(poll.left);
-                    }
-                    if (poll.right != null) {
-                        poll.right.val += poll.val;
-                        queue.offer(poll.right);
-                    }
-                }
-            }
-
-            return false;
-        }
+      }
+      
+      return false;
     }
-//leetcode submit region end(Prohibit modification and deletion)
+    
+    // BFS+queue
+    public boolean hasPathSum_BFS(TreeNode root, int targetSum) {
+      if (root == null) return false;
+      Queue<TreeNode> queue = new LinkedList<>();
+      queue.offer(root);
+      
+      while (!queue.isEmpty()) {
+        TreeNode poll = queue.poll();
+        if (poll.left == null && poll.right == null) {
+          if (poll.val == targetSum)
+            return true;
+        } else {
+          if (poll.left != null) {
+            poll.left.val += poll.val;
+            queue.offer(poll.left);
+          }
+          if (poll.right != null) {
+            poll.right.val += poll.val;
+            queue.offer(poll.right);
+          }
+        }
+      }
+      
+      return false;
+    }
+  }
+  // leetcode submit region end(Prohibit modification and deletion)
 }
