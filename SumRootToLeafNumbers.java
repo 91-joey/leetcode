84c293583e01b9dacff4116ba81af9aed8b00741
joeyLocal <joey.91@qq.com>
2023-02-02 12:40:13 +0800

    1129.颜色交替的最短路径

---
+++ SumRootToLeafNumbers.java
@@ -0,0 +1,60 @@
+package org.example.leetcode.problems;
+
+import org.example.leetcode.problems._3_common.entity.tree.TreeNode;
+
+/**
+ * 129.求根节点到叶节点数字之和 <br>
+ * 开题时间：2023-02-02 11:42:27
+ */
+public class SumRootToLeafNumbers {
+  public static void main(String[] args) {
+    Solution solution = new SumRootToLeafNumbers().new Solution();
+    System.out.println(solution);
+  }
+  
+  // leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+    int sum = 0;
+    
+    // 无返回值 dfs
+    public int sumNumbers9(TreeNode root) {
+      dfs9(root, root.val);
+      
+      return sum;
+    }
+    
+    private void dfs9(TreeNode root, int path) {
+      if (root.left == null && root.right == null) {
+        sum += path;
+        return;
+      }
+      
+      for (TreeNode node : new TreeNode[]{root.left, root.right}) {
+        if (node != null) {
+          dfs9(node, path * 10 + node.val);
+        }
+      }
+    }
+  
+  
+    // 有返回值 dfs
+    public int sumNumbers(TreeNode root) {
+      return dfs(root, 0);
+    }
+    
+    private int dfs(TreeNode root, int prevSum) {
+      if (root == null) {
+        return 0;
+      }
+      
+      prevSum = prevSum * 10 + root.val;
+      if (root.left == null && root.right == null) {
+        return prevSum;
+      }
+      
+      return dfs(root.left, prevSum)
+          + dfs(root.right, prevSum);
+    }
+  }
+  // leetcode submit region end(Prohibit modification and deletion)
+}
---
+++ ThreeEtpl5.java
@@ -0,0 +1,60 @@
+package org.example.leetcode.problems;
+
+import org.example.leetcode.problems._3_common.entity.tree.TreeNode;
+
+/**
+ * 剑指 Offer II 049.从根节点到叶节点的路径数字之和 <br>
+ * 开题时间：2023-02-02 11:42:19
+ */
+public class ThreeEtpl5 {
+  public static void main(String[] args) {
+    Solution solution = new ThreeEtpl5().new Solution();
+    System.out.println(solution);
+  }
+  
+  // leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+    int sum = 0;
+    
+    // 无返回值 dfs
+    public int sumNumbers9(TreeNode root) {
+      dfs9(root, root.val);
+      
+      return sum;
+    }
+    
+    private void dfs9(TreeNode root, int path) {
+      if (root.left == null && root.right == null) {
+        sum += path;
+        return;
+      }
+      
+      for (TreeNode node : new TreeNode[]{root.left, root.right}) {
+        if (node != null) {
+          dfs9(node, path * 10 + node.val);
+        }
+      }
+    }
+    
+    
+    // 有返回值 dfs
+    public int sumNumbers(TreeNode root) {
+      return dfs(root, 0);
+    }
+    
+    private int dfs(TreeNode root, int prevSum) {
+      if (root == null) {
+        return 0;
+      }
+      
+      prevSum = prevSum * 10 + root.val;
+      if (root.left == null && root.right == null) {
+        return prevSum;
+      }
+      
+      return dfs(root.left, prevSum)
+          + dfs(root.right, prevSum);
+    }
+  }
+  // leetcode submit region end(Prohibit modification and deletion)
+}
