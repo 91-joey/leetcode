974815df2ec75f8a27ab24134bf73777794bae32
joeyLocal <joey.91@qq.com>
2023-02-04 12:41:05 +0800

    105.从前序与中序遍历序列构造二叉树

--- _1_dataStructure/tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ _1_dataStructure/tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -40,116 +40,117 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
-//105.从前序与中序遍历序列构造二叉树
-//开题时间：2022-09-16 08:32:45
+// 105.从前序与中序遍历序列构造二叉树
+// 开题时间：2022-09-16 08:32:45
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
-    public static void main(String[] args) {
-        Solution solution = new ConstructBinaryTreeFromPreorderAndInorderTraversal().new Solution();
-//        System.out.println(solution.buildTree3(new int[]{3, 9, 20, 15, 7}, new int[]{9, 3, 15, 20, 7}));
-        System.out.println(solution.buildTree3(new int[]{3, 9, 15, 10, 20, 7, 5, 8, 4}, new int[]{15, 9, 10, 3, 20, 5, 7, 8, 4}));
+  public static void main(String[] args) {
+    Solution solution = new ConstructBinaryTreeFromPreorderAndInorderTraversal().new Solution();
+    //        System.out.println(solution.buildTree3(new int[]{3, 9, 20, 15, 7}, new int[]{9, 3, 15, 20, 7}));
+    System.out.println(solution.buildTree3(new int[]{3, 9, 15, 10, 20, 7, 5, 8, 4}, new int[]{15, 9, 10, 3, 20, 5, 7, 8, 4}));
+  }
+  
+  // leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+    // 递归
+    Map<Integer, Integer> val2idx = new HashMap<>();
+    int[] preorder;
+    int preIdx;
+    
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+      this.preorder = preorder;
+      preIdx = 0;
+      
+      int idx = 0;
+      for (int val : inorder)
+        val2idx.put(val, idx++);
+      
+      return helper(0, inorder.length - 1);
     }
-
-    //leetcode submit region begin(Prohibit modification and deletion)
-    class Solution {
-        //递归
-        Map<Integer, Integer> val2idx = new HashMap<>();
-        int[] preorder;
-        int preIdx;
-
-        public TreeNode buildTree(int[] preorder, int[] inorder) {
-            this.preorder = preorder;
-            preIdx = 0;
-
-            int idx = 0;
-            for (int val : inorder)
-                val2idx.put(val, idx++);
-
-            return helper(0, inorder.length - 1);
-        }
-
-        //迭代
-        private TreeNode helper(int l, int r) {
-            if (l > r) return null;
-
-            int rootVal = preorder[preIdx++];
-            Integer rootIdx = val2idx.get(rootVal);
-            TreeNode root = new TreeNode(rootVal);
-            root.left = helper(l, rootIdx - 1);
-            root.right = helper(rootIdx + 1, r);
-
-            return root;
-        }
-
-        /*
-                     3
-                    / \
-                   9  20
-                  / \   \
-                 15 10   7
-                        / \
-                       5   8
-                            \
-                             4
-                preorder =  [3, 9, 15, 10, 20, 7, 5, 8, 4]
-                inorder   = [15, 9, 10, 3, 20, 5, 7, 8, 4]
-        */
-        //迭代
-        public TreeNode buildTree2(int[] preorder, int[] inorder) {
-            int length = preorder.length;
-            int inIdx = 0;
-            TreeNode root = new TreeNode(preorder[0]);
-            Deque<TreeNode> stack = new LinkedList<>();
-            stack.push(root);
-
-            for (int i = 1; i < length; i++) {
-                TreeNode peek = stack.peek();
-                TreeNode node = new TreeNode(preorder[i]);
-                if (peek.val != inorder[inIdx]) {
-                    peek.left = node;
-                } else {
-                    while (!stack.isEmpty() && stack.peek().val == inorder[inIdx]) {
-                        peek = stack.pop();
-                        inIdx++;
-                    }
-                    peek.right = node;
-                }
-                stack.push(node);
-            }
-
-            return root;
-        }
-
-        //高分解（递归）
-        //todo
-        private int in;
-        private int pre;
-        public TreeNode buildTree3(int[] preorder, int[] inorder) {
-            return build(preorder, inorder, (long) Integer.MAX_VALUE + 1);
-        }
-
-        /*
-                     3
-                    / \
-                   9  20
-                  / \   \
-                 15 10   7
-                        / \
-                       5   8
-                            \
-                             4
-                preorder =  [3, 9, 15, 10, 20, 7, 5, 8, 4]
-                inorder   = [15, 9, 10, 3, 20, 5, 7, 8, 4]
-        */
-        private TreeNode build(int[] preorder, int[] inorder, long stop) {
-            if (in < inorder.length && inorder[in] != stop) {
-                TreeNode root = new TreeNode(preorder[pre++]);
-                root.left = build(preorder, inorder, root.val);
-                in++;
-                root.right = build(preorder, inorder, stop);
-                return root;
-            }
-            return null;
-        }
+    
+    // 迭代
+    private TreeNode helper(int l, int r) {
+      if (l > r) return null;
+      
+      int rootVal = preorder[preIdx++];
+      Integer rootIdx = val2idx.get(rootVal);
+      TreeNode root = new TreeNode(rootVal);
+      root.left = helper(l, rootIdx - 1);
+      root.right = helper(rootIdx + 1, r);
+      
+      return root;
     }
-//leetcode submit region end(Prohibit modification and deletion)
+    
+    /*
+                 3
+                / \
+               9  20
+              / \   \
+             15 10   7
+                    / \
+                   5   8
+                        \
+                         4
+            preorder =  [3, 9, 15, 10, 20, 7, 5, 8, 4]
+            inorder   = [15, 9, 10, 3, 20, 5, 7, 8, 4]
+    */
+    // 迭代
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
+      int length = preorder.length;
+      int inIdx = 0;
+      TreeNode root = new TreeNode(preorder[0]);
+      Deque<TreeNode> stack = new LinkedList<>();
+      stack.push(root);
+      
+      for (int i = 1; i < length; i++) {
+        TreeNode peek = stack.peek();
+        TreeNode node = new TreeNode(preorder[i]);
+        if (peek.val != inorder[inIdx]) {
+          peek.left = node;
+        } else {
+          while (!stack.isEmpty() && stack.peek().val == inorder[inIdx]) {
+            peek = stack.pop();
+            inIdx++;
+          }
+          peek.right = node;
+        }
+        stack.push(node);
+      }
+      
+      return root;
+    }
+    
+    // 高分解（递归）
+    // todo
+    private int in;
+    private int pre;
+    
+    public TreeNode buildTree3(int[] preorder, int[] inorder) {
+      return build(preorder, inorder, (long) Integer.MAX_VALUE + 1);
+    }
+    
+    /*
+                 3
+                / \
+               9  20
+              / \   \
+             15 10   7
+                    / \
+                   5   8
+                        \
+                         4
+            preorder =  [3, 9, 15, 10, 20, 7, 5, 8, 4]
+            inorder   = [15, 9, 10, 3, 20, 5, 7, 8, 4]
+    */
+    private TreeNode build(int[] preorder, int[] inorder, long stop) {
+      if (in < inorder.length && inorder[in] != stop) {
+        TreeNode root = new TreeNode(preorder[pre++]);
+        root.left = build(preorder, inorder, root.val);
+        in++;
+        root.right = build(preorder, inorder, stop);
+        return root;
+      }
+      return null;
+    }
+  }
+  // leetcode submit region end(Prohibit modification and deletion)
 }
--- _2_algorithm/divideAndConquer/problems/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ _2_algorithm/divideAndConquer/problems/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -38,67 +38,99 @@ import org.example.leetcode.problems._3_common.entity.tree.TreeNode;
 import java.util.HashMap;
 import java.util.Map;
 
-//105.从前序与中序遍历序列构造二叉树
-//开题时间：2022-11-16 14:35:15
+// 105.从前序与中序遍历序列构造二叉树
+// 开题时间：2022-11-16 14:35:15
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
-    public static void main(String[] args) {
-        Solution solution = new ConstructBinaryTreeFromPreorderAndInorderTraversal().new Solution();
-//        System.out.println(solution.buildTree(new int[]{1, 2}, new int[]{2, 1}));
-        System.out.println(solution.buildTree(new int[]{3, 2, 1, 4}, new int[]{1, 2, 3, 4}));
-//        System.out.println(solution.buildTree(new int[]{3, 9, 20, 15, 7}, new int[]{9, 3, 15, 20, 7}));
+  public static void main(String[] args) {
+    Solution solution = new ConstructBinaryTreeFromPreorderAndInorderTraversal().new Solution();
+    //        System.out.println(solution.buildTree(new int[]{1, 2}, new int[]{2, 1}));
+    System.out.println(solution.buildTree(new int[]{3, 2, 1, 4}, new int[]{1, 2, 3, 4}));
+    //        System.out.println(solution.buildTree(new int[]{3, 9, 20, 15, 7}, new int[]{9, 3, 15, 20, 7}));
+  }
+  
+  // leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+    Map<Integer, Integer> val2idxPre = new HashMap<>();
+    Map<Integer, Integer> val2idxIn = new HashMap<>();
+    
+    public TreeNode buildTreeX(int[] preorder, int[] inorder) {
+      for (int i = 0; i < preorder.length; i++)
+        val2idxPre.put(preorder[i], i);
+      for (int i = 0; i < inorder.length; i++)
+        val2idxIn.put(inorder[i], i);
+      
+      return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
-
-    //leetcode submit region begin(Prohibit modification and deletion)
-    class Solution {
-        Map<Integer, Integer> val2idxPre = new HashMap<>();
-        Map<Integer, Integer> val2idxIn = new HashMap<>();
-
-        public TreeNode buildTreeX(int[] preorder, int[] inorder) {
-            for (int i = 0; i < preorder.length; i++)
-                val2idxPre.put(preorder[i], i);
-            for (int i = 0; i < inorder.length; i++)
-                val2idxIn.put(inorder[i], i);
-
-            return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
-        }
-
-        private TreeNode buildTree(int[] preorder, int l1, int r1, int[] inorder, int l2, int r2) {
-            if (l1 > r1 || l2 > r2)
-                return null;
-
-            TreeNode head = new TreeNode(preorder[l1]);
-            int leftMostIn = val2idxIn.get(preorder[l1]) - 1;
-            //前序、中序遍历的左子节点序列的最后一个值并不相同（考虑左子节点没有右子节点的情况，此时前序遍历最后一个值为左下角的值，中序遍历为根节点的左子节点）
-            int leftMostPre = leftMostIn < l2 ? l1 : val2idxPre.get(inorder[leftMostIn]);
-//            if (leftMostIn >= l2 && leftMostIn <= r2)
-            head.left = buildTree(preorder, l1 + 1, leftMostPre, inorder, l2, leftMostIn);
-//            if (leftMostPre >= l1 - 1 && leftMostPre < r1)
-            head.right = buildTree(preorder, leftMostPre + 1, r1, inorder, leftMostIn + 2, r2);
-
-            return head;
-        }
-
-        public TreeNode buildTree(int[] preorder, int[] inorder) {
-            int len = inorder.length;
-            for (int i = 0; i < len; i++)
-                val2idxIn.put(inorder[i], i);
-
-            return buildTree(preorder, inorder, 0, len - 1, 0, len - 1);
-        }
-
-        private TreeNode buildTree(int[] preorder, int[] inorder, int lPre, int rPre, int lIn, int rIn) {
-            if (lPre > rPre)
-                return null;
-
-            TreeNode root = new TreeNode(preorder[lPre]);
-            int rootIdxIn = val2idxIn.get(preorder[lPre]);
-            //前序/中序 遍历的 左/右 子节点序列 的长度是一样的，所以不用再去求前序遍历中的 左/右 子节点序列边界值
-            int leftMost = lPre + rootIdxIn - lIn;
-            root.left = buildTree(preorder, inorder, lPre + 1, leftMost, lIn, rootIdxIn - 1);
-            root.right = buildTree(preorder, inorder, leftMost + 1, rPre, rootIdxIn + 1, rIn);
-
-            return root;
-        }
+    
+    private TreeNode buildTree(int[] preorder, int l1, int r1, int[] inorder, int l2, int r2) {
+      if (l1 > r1 || l2 > r2)
+        return null;
+      
+      TreeNode head = new TreeNode(preorder[l1]);
+      int leftMostIn = val2idxIn.get(preorder[l1]) - 1;
+      // 前序、中序遍历的左子节点序列的最后一个值并不相同（考虑左子节点没有右子节点的情况，此时前序遍历最后一个值为左下角的值，中序遍历为根节点的左子节点）
+      int leftMostPre = leftMostIn < l2 ? l1 : val2idxPre.get(inorder[leftMostIn]);
+      //            if (leftMostIn >= l2 && leftMostIn <= r2)
+      head.left = buildTree(preorder, l1 + 1, leftMostPre, inorder, l2, leftMostIn);
+      //            if (leftMostPre >= l1 - 1 && leftMostPre < r1)
+      head.right = buildTree(preorder, leftMostPre + 1, r1, inorder, leftMostIn + 2, r2);
+      
+      return head;
     }
-//leetcode submit region end(Prohibit modification and deletion)
+    
+    // 递归 + 哈希表 + 遍历寻找前序遍历中右子树的起点 n^2 n
+    public TreeNode buildTree9(int[] preorder, int[] inorder) {
+      HashMap<Integer, Integer> map = new HashMap<>();
+      for (int i = 0; i < inorder.length; i++) {
+        map.put(inorder[i], i);
+      }
+      return buildTree(preorder, map, 0, inorder.length - 1);
+    }
+    
+    private TreeNode buildTree(int[] preorder, HashMap<Integer, Integer> map, int lPre, int rPre) {
+      if (lPre > rPre) {
+        return null;
+      }
+      int rootIn = map.get(preorder[lPre]);
+      // 可优化：前中序遍历的 左右子节点序列 的长度是一样的
+      int preRightStart = lPre + 1;
+      while (preRightStart <= rPre && map.get(preorder[preRightStart]) < rootIn) {
+        preRightStart++;
+      }
+      return new TreeNode(preorder[lPre],
+          buildTree(preorder, map, lPre + 1, preRightStart - 1),
+          buildTree(preorder, map, preRightStart, rPre)
+      );
+    }
+    
+    /*
+     * ☆☆☆☆☆ 递归 + 哈希表 n n
+     * 前序遍历：[ 根节点, [左子树的前序遍历结果], [右子树的前序遍历结果] ]   特性：前序遍历的第一个值为根节点
+     * 中序遍历：[ [左子树的中序遍历结果], 根节点, [右子树的中序遍历结果] ]
+     *
+     * 在中序遍历中定位根节点的索引为 rootIn，我们可以预处理出中序遍历值和索引的映射
+     * 由于前序遍历和中序遍历的长度相同，因此根节点的左右子树亦是长度相同。知道中序遍历中根节点的索引 rootIn，即可求出前序遍历中左右子树序列的范围。
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+      int len = inorder.length;
+      for (int i = 0; i < len; i++)
+        val2idxIn.put(inorder[i], i);
+      
+      return buildTree(preorder, 0, len - 1, 0);
+    }
+    
+    private TreeNode buildTree(int[] preorder, int lPre, int rPre, int lIn) {
+      if (lPre > rPre)
+        return null;
+      
+      int rootIn = val2idxIn.get(preorder[lPre]);
+      int leftMostPre = lPre + rootIn - lIn;
+      
+      return new TreeNode(preorder[lPre],
+          buildTree(preorder, lPre + 1, leftMostPre, lIn),
+          buildTree(preorder, leftMostPre + 1, rPre, rootIn + 1)
+      );
+    }
+  }
+  // leetcode submit region end(Prohibit modification and deletion)
 }
