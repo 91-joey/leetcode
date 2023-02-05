1c4585671875edda864300c82d097a4a65099216
joeyLocal <joey.91@qq.com>
2023-02-05 14:07:30 +0800

    6348. Take Gifts From the Richest Pile

--- _9_contest/week331/T1.java
+++ _9_contest/week331/T1.java
@@ -3,7 +3,7 @@ package org.example.leetcode.problems._9_contest.week331;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-//
+//6348. Take Gifts From the Richest Pile
 public class T1 {
   public static void main(String[] args) {
     Solution solution = new T1().new Solution();
@@ -11,22 +11,18 @@ public class T1 {
   }
   
   class Solution {
+    // 大顶堆
     public long pickGifts(int[] gifts, int k) {
-      PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+      PriorityQueue<Integer> pq = new PriorityQueue<>(gifts.length, Comparator.reverseOrder());
       for (int gift : gifts) {
         pq.offer(gift);
       }
       
       for (int i = 0; i < k; i++) {
-        Integer poll = pq.poll();
-        pq.offer((int) Math.sqrt(poll));
+        pq.offer((int) Math.sqrt(pq.poll()));
       }
       
-      long ans = 0;
-      while (!pq.isEmpty()) {
-        ans += pq.poll();
-      }
-      return ans;
+      return pq.stream().mapToLong(Integer::intValue).sum();
     }
   }
 }
