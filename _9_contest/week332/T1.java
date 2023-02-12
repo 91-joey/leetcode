5675f20c60820f0bf9b750eccd8efe32daca65a2
joeyLocal <joey.91@qq.com>
2023-02-12 14:12:29 +0800

    6354. Find the Array Concatenation Value

--- _9_contest/week332/T1.java
+++ _9_contest/week332/T1.java
@@ -1,20 +1,42 @@
 package org.example.leetcode.problems._9_contest.week332;
 
-//
+// 6354. Find the Array Concatenation Value
 public class T1 {
   public static void main(String[] args) {
     Solution solution = new T1().new Solution();
-    System.out.println(solution);
+    System.out.println(solution.findTheArrayConcVal(new int[]{5, 14, 13, 8, 12}));
   }
   
   class Solution {
-    
-    public long findTheArrayConcVal(int[] nums) {
+    // 字符串拼接和解析
+    public long findTheArrayConcVal9(int[] nums) {
       long ans = 0;
       for (int l = 0, r = nums.length - 1; l <= r; l++, r--) {
-        String a = String.valueOf(nums[l]);
-        String b = String.valueOf(nums[r]);
-        ans += l == r ? Long.parseLong(b) : Long.parseLong(a + b);
+        ans += l == r ?
+            nums[l] :
+            Long.parseLong("" + nums[l] + nums[r]);
+      }
+      return ans;
+    }
+    
+    // 数位运算
+    public long findTheArrayConcVal(int[] nums) {
+      long ans = 0;
+      
+      int l = 0;
+      int r = nums.length - 1;
+      while (l < r) {
+        int x = nums[l++];
+        int y = nums[r];
+        while (y > 0) {
+          x *= 10;
+          y /= 10;
+        }
+        ans += x + nums[r--];
+      }
+      
+      if (l == r) {
+        ans += nums[l];
       }
       return ans;
     }
