93d9fcef9652a1d1102933c6a2eb538ad20c1897
joeyLocal <joey.91@qq.com>
2023-02-15 17:34:40 +0800

    1020.飞地的数量

--- _1_dataStructure/graph/NumberOfEnclaves.java
+++ _1_dataStructure/graph/NumberOfEnclaves.java
@@ -38,8 +38,11 @@ package org.example.leetcode.problems._1_dataStructure.graph;
 
 import org.example.leetcode.problems._3_common.tool.Tools;
 
-// 1020.飞地的数量
-// 开题时间：2023-01-03 16:07:09
+/**
+ * 1020.飞地的数量 <br>
+ * 和这题即为相似：130.被围绕的区域
+ * 开题时间：2023-02-15 17:10:46
+ */
 public class NumberOfEnclaves {
   public static void main(String[] args) {
     Solution solution = new NumberOfEnclaves().new Solution();
@@ -50,7 +53,7 @@ public class NumberOfEnclaves {
   class Solution {
     public static final int[] DIRS = {1, 0, -1, 0, 1};
     
-    public int numEnclaves(int[][] grid) {
+    public int numEnclaves99(int[][] grid) {
       int cnt = 0;
       for (int i = 0; i < grid.length; i++)
         for (int j = 0; j < grid[0].length; j++)
@@ -78,6 +81,85 @@ public class NumberOfEnclaves {
       }
       return new Island(closed, cnt);
     }
+  
+  
+    // ☆☆☆☆☆ dfs
+    public int numEnclaves9(int[][] grid) {
+      int n = grid.length;
+      int m = grid[0].length;
+      boolean[][] vis = new boolean[n][m];
+    
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          if ((i == 0 || j == 0 || i == n - 1 || j == m - 1) && !vis[i][j] && grid[i][j] == 1) {
+            dfs(grid, i, j, vis);
+          }
+        }
+      }
+    
+      int cnt = 0;
+      for (int i = 1; i < n - 1; i++) {
+        for (int j = 1; j < m - 1; j++) {
+          if (!vis[i][j] && grid[i][j] == 1) {
+            cnt++;
+          }
+        }
+      }
+    
+      return cnt;
+    }
+  
+    private void dfs(int[][] arr, int r, int c, boolean[][] vis) {
+      vis[r][c] = true;
+    
+      for (int i = 0; i < 4; i++) {
+        int nr = r + DIRS[i];
+        int nc = c + DIRS[i + 1];
+        if (0 <= nr && nr < arr.length && 0 <= nc && nc < arr[0].length &&
+            !vis[nr][nc] && arr[nr][nc] == 1)
+          dfs(arr, nr, nc, vis);
+      }
+    }
+  
+    // ☆☆☆☆ 并查集
+    public int numEnclaves(int[][] grid) {
+      int n = grid.length;
+      int m = grid[0].length;
+      // 虚拟节点
+      int dummy = n * m;
+      UnionFind uf = new UnionFind(dummy + 1);
+    
+      for (int r = 0; r < n; r++) {
+        for (int c = 0; c < m; c++) {
+          if (grid[r][c] == 1) {
+            if ((r == 0 || c == 0 || r == n - 1 || c == m - 1)) {
+              // 边界上的 1 都与 dummy 合并成一个连通区域
+              uf.union(r * m + c, dummy);
+            } else {
+              // 相邻的 1 合并
+              for (int i = 0; i < 4; i++) {
+                int nr = r + DIRS[i];
+                int nc = c + DIRS[i + 1];
+                if (0 <= nr && nr < n && 0 <= nc && nc < m && grid[nr][nc] == 1) {
+                  uf.union(nr * m + nc, r * m + c);
+                }
+              }
+            }
+          }
+        }
+      }
+    
+      int cnt=0;
+      for (int r = 0; r < n; r++) {
+        for (int c = 0; c < m; c++) {
+          if (grid[r][c] == 1 && !uf.connected(r * m + c, dummy)) {
+            cnt++;
+          }
+        }
+      }
+    
+      return cnt;
+    }
   }
   
   class Island {
@@ -87,6 +169,41 @@ public class NumberOfEnclaves {
     public Island(boolean closed, int cnt) {
       this.closed = closed;
       this.cnt = cnt;
+    }
+  }
+  
+  class UnionFind {
+    int[] root;
+    int[] rank;
+    
+    public UnionFind(int size) {
+      root = new int[size];
+      rank = new int[size];
+      for (int i = 0; i < size; i++)
+        root[i] = i;
+    }
+    
+    public int find(int x) {
+      if (x == root[x])
+        return x;
+      return root[x] = find(root[x]);
+    }
+    
+    public void union(int x, int y) {
+      int rootX = find(x);
+      int rootY = find(y);
+      if (rootX != rootY)
+        if (rank[rootX] == rank[rootY]) {
+          root[rootX] = rootY;
+          rank[rootY]++;
+        } else if (rank[rootX] < rank[rootY])
+          root[rootX] = rootY;
+        else
+          root[rootY] = rootX;
+    }
+    
+    public boolean connected(int x, int y) {
+      return find(x) == find(y);
     }
   }
   // leetcode submit region end(Prohibit modification and deletion)
