81da998617a427fd33287e29e8075cf567046205
joeyLocal <joey.91@qq.com>
2023-02-16 09:41:25 +0800

    2341.数组能形成多少数对

---
+++ _1_dataStructure/hashtable/MaximumNumberOfPairsInArray.java
@@ -0,0 +1,28 @@
+package org.example.leetcode.problems._1_dataStructure.hashtable;
+
+import java.util.Arrays;
+
+/**
+ * 2341.数组能形成多少数对 <br>
+ * 开题时间：2023-02-16 09:33:43
+ */
+public class MaximumNumberOfPairsInArray {
+  public static void main(String[] args) {
+    Solution solution = new MaximumNumberOfPairsInArray().new Solution();
+    System.out.println(solution);
+  }
+  
+  // leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+    // 哈希表
+    public int[] numberOfPairs(int[] nums) {
+      int[] freq = new int[101];
+      for (int x : nums) {
+        freq[x]++;
+      }
+      int leftover = Arrays.stream(freq).map(x -> x % 2).sum();
+      return new int[]{(nums.length - leftover) / 2, leftover};
+    }
+  }
+  // leetcode submit region end(Prohibit modification and deletion)
+}
