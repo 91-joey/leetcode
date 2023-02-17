43f45c3c1c8c5a766f93a688e491a85bef2fff70
joeyLocal <joey.91@qq.com>
2023-02-17 15:56:07 +0800

    1124.表现良好的最长时间段

--- _1_dataStructure/monotonic/LongestWellPerformingInterval.java
+++ _1_dataStructure/monotonic/LongestWellPerformingInterval.java
@@ -1,5 +1,6 @@
 package org.example.leetcode.problems._1_dataStructure.monotonic;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -28,8 +29,8 @@ public class LongestWellPerformingInterval {
       return ans;
     }
     
-    // 前缀和 + 哈希表
-    public int longestWPI(int[] hours) {
+    // ☆☆☆☆☆ 前缀和 + 哈希表
+    public int longestWPI7(int[] hours) {
       int ans = 0;
       HashMap<Integer, Integer> val2firstIdx = new HashMap<>();
       for (int i = 0, sum = 0; i < hours.length; i++) {
@@ -39,6 +40,51 @@ public class LongestWellPerformingInterval {
             i - val2firstIdx.getOrDefault(sum - 1, i));
         val2firstIdx.putIfAbsent(sum, i);
       }
+      return ans;
+    }
+  
+    // 前缀和 + 数组
+    public int longestWPI8(int[] hours) {
+      int n = hours.length;
+      int[] firstIdx = new int[n + 2];
+      Arrays.fill(firstIdx, -1);
+      int ans = 0;
+      for (int i = 0, sum = 0; i < n; i++) {
+        sum += (hours[i] > 8 ? 1 : -1);
+        if (sum > 0) {
+          ans = Math.max(ans, i + 1);
+        } else {
+          if (firstIdx[sum + n] != -1) {
+            ans = Math.max(ans, i - firstIdx[sum + n]);
+          }
+          if (firstIdx[sum + 1 + n] == -1) {
+            firstIdx[sum + 1 + n] = i;
+          }
+        }
+      }
+      return ans;
+    }
+  
+    // ☆☆☆☆ 前缀和（取反） + 数组（存{索引+1}）
+    public int longestWPI(int[] hours) {
+      int n = hours.length;
+      int[] pos = new int[n + 2];
+      int ans = 0;
+    
+      for (int i = 1, sum = 0; i <= n; i++) {
+        sum += hours[i - 1] > 8 ? -1 : +1;
+        if (sum < 0) {
+          ans = i;
+        } else {
+          if (pos[sum + 1] > 0) {
+            ans = Math.max(ans, i - pos[sum + 1]);
+          }
+          if (pos[sum] == 0) {
+            pos[sum] = i;
+          }
+        }
+      }
+    
       return ans;
     }
     
