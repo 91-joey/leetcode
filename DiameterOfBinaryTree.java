580c73c7b213bee03346da373d6ce88c30755511
joeyLocal <joey.91@qq.com>
2023-02-18 15:35:08 +0800

    543.二叉树的直径

--- DiameterOfBinaryTree.java
+++ DiameterOfBinaryTree.java
@@ -20,14 +20,35 @@ public class DiameterOfBinaryTree {
     List<List<Integer>> graph = new ArrayList<List<Integer>>();
     int ans = 0;
     
-    public int diameterOfBinaryTree(TreeNode root) {
+    /*
+     * ☆☆☆☆☆ 树形dp
+     * 任意一条路径都可以看成是某一个节点的「左子树的路径」「右子树的路径」拼接在一起，某一子树的路径可能为空
+     * 不用考虑其余部分的路径（上溯父节点），因为这种情况也可以作为「某个节点的左右子树路径和」考虑
+     */
+    public int diameterOfBinaryTree9(TreeNode root) {
       buildGraph(root, 0, -1);
       
       dfs(0, -1);
-  
+      
       return ans;
     }
     
+    public int diameterOfBinaryTree(TreeNode root) {
+      dfs(root);
+      return ans;
+    }
+    
+    private int dfs(TreeNode root) {
+      if (root == null) {
+        return 0;
+      }
+      int l = dfs(root.left);
+      int r = dfs(root.right);
+      ans = Math.max(ans, l + r);
+      return Math.max(l, r) + 1;
+    }
+    
+    // 建图 + 树形dp
     private void buildGraph(TreeNode root, int v, int u) {
       graph.add(new ArrayList<Integer>());
       if (u != -1) {
@@ -37,11 +58,11 @@ public class DiameterOfBinaryTree {
       if (root.left != null) {
         buildGraph(root.left, graph.size(), v);
       }
-      if (root.right!= null) {
+      if (root.right != null) {
         buildGraph(root.right, graph.size(), v);
       }
     }
-  
+    
     /**
      * 求以 u 为根节点、以 fa 为父节点（不能返回父节点）的最大路径长度
      *
@@ -51,7 +72,7 @@ public class DiameterOfBinaryTree {
     private int dfs(int u, int fa) {
       int max1 = 0; // 最大值
       int max2 = 0; // 次大值
-    
+      
       for (Integer v : graph.get(u)) {
         if (v == fa) {
           continue;
@@ -66,7 +87,7 @@ public class DiameterOfBinaryTree {
       }
       // 经过 u 的最长链的长度 = 以 u 为根节点的最大路径长度 +　以 u 为根节点的次大路径长度
       ans = Math.max(ans, max1 + max2);
-    
+      
       return max1 + 1;
     }
   }
