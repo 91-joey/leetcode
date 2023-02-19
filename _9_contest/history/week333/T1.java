6a77904de5fb4c7b55b82a507a1792fcbf680b6d
joeyLocal <joey.91@qq.com>
2023-02-19 21:13:16 +0800

    6362. Merge Two 2D Arrays by Summing Values

--- _9_contest/history/week333/T1.java
+++ _9_contest/history/week333/T1.java
@@ -2,7 +2,7 @@ package org.example.leetcode.problems._9_contest.history.week333;
 
 import java.util.ArrayList;
 
-//
+// 6362. Merge Two 2D Arrays by Summing Values
 public class T1 {
   public static void main(String[] args) {
     Solution solution = new T1().new Solution();
@@ -10,7 +10,7 @@ public class T1 {
   }
   
   class Solution {
-    
+    // 归并排序
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
       ArrayList<int[]> list = new ArrayList<>();
       for (int i = 0, j = 0; i < nums1.length || j < nums2.length; ) {
@@ -23,9 +23,7 @@ public class T1 {
         } else if (nums1[i][0] > nums2[j][0]) {
           list.add(nums2[j++]);
         } else {
-          list.add(new int[]{nums1[i][0], nums1[i][1] + nums2[j][1]});
-          i++;
-          j++;
+          list.add(new int[]{nums1[i][0], nums1[i++][1] + nums2[j++][1]});
         }
       }
       return list.toArray(new int[list.size()][]);
