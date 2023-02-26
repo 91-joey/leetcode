2336e28a565501f1a364a10f9348cf16ef89d0bf
joey <joey.91@qq.com>
2023-02-26 14:36:00 +0800

    6368. Find the Divisibility Array of a String

--- _9_contest/week334/T2.java
+++ _9_contest/week334/T2.java
@@ -3,7 +3,7 @@ package _9_contest.week334;
 import java.math.BigInteger;
 import java.util.Arrays;
 
-//
+// 6368. Find the Divisibility Array of a String
 public class T2 {
   public static void main(String[] args) {
     Solution solution = new T2().new Solution();
@@ -11,36 +11,43 @@ public class T2 {
     // System.out.println(Arrays.toString(solution.divisibilityArray("1010", 10)));
   }
   
+  
+  // import java.math.BigInteger;
+  
   class Solution {
-    
-    public int[] divisibilityArray(String word, int m) {
+    /*
+     * 暴力优化仍旧TLE
+     *  [0:i-1]所代表的数能被 m 整除，则[0:i]所代表的数能否被 m 整除 取决于 nums[i] 能否被 m 整除
+     */
+    public int[] divisibilityArrayTLE(String word, int m) {
       int n = word.length();
       int[] ans = new int[n];
       int digit = word.charAt(0) - '0';
       ans[0] = digit % m == 0 ? 1 : 0;
       BigInteger sum = BigInteger.valueOf(digit);
-      for (int i = 1, gcd = 0; i < word.length(); i++) {
-        sum = sum.multiply(BigInteger.TEN);
+      
+      for (int i = 1; i < n; i++) {
         digit = word.charAt(i) - '0';
+        sum = sum.multiply(BigInteger.TEN).add(BigInteger.valueOf(digit));
         if (ans[i - 1] == 1) {
           ans[i] = digit % m == 0 ? 1 : 0;
-          sum = sum.add(BigInteger.valueOf(digit));
         } else {
-          sum = sum.add(BigInteger.valueOf(digit));
           ans[i] = sum.mod(BigInteger.valueOf(m)).equals(BigInteger.ZERO) ? 1 : 0;
-          // ans[i] = sum.gcd(BigInteger.valueOf(digit)).mod(BigInteger.valueOf(m)).equals(BigInteger.ZERO) ? 1 : 0;
         }
       }
+      
       return ans;
     }
     
-    public static int gcd(int a, int b) {
-      while (a != 0) {
-        int tmp = a;
-        a = b % a;
-        b = tmp;
+    // ☆☆☆☆☆ 取余递推
+    public int[] divisibilityArray(String word, int m) {
+      int n = word.length();
+      int[] ans = new int[n];
+      for (int i = 0, remainder = 0; i < n; i++) {
+        remainder = (int) ((10L * remainder + word.charAt(i) - '0') % m);
+        ans[i] = remainder == 0 ? 1 : 0;
       }
-      return b;
+      return ans;
     }
   }
 }
