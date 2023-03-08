e374560f33f518eac4927a22de6d4016ec58bc8e
joey <joey.91@qq.com>
2023-03-08 17:45:27 +0800

    494.目标和

--- _1_dataStructure/queueAndStack/TargetSum.java
+++ _1_dataStructure/queueAndStack/TargetSum.java
@@ -45,8 +45,8 @@
 package _1_dataStructure.queueAndStack;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Queue;
 
 // 494.目标和
@@ -54,14 +54,14 @@ import java.util.Queue;
 public class TargetSum {
   public static void main(String[] args) {
     Solution solution = new TargetSum().new Solution();
-    System.out.println(solution.findTargetSumWays2(new int[]{1, 1, 1, 1, 1}, 3));
+    // System.out.println(solution.findTargetSumWays2(new int[]{1, 1, 1, 1, 1}, 3));
     //        System.out.println(solution.findTargetSumWays(new int[]{1, 0}, 1));
   }
   
   // leetcode submit region begin(Prohibit modification and deletion)
   class Solution {
     // 1.BFS+Queue   2^n 2^n
-    public int findTargetSumWays(int[] nums, int target) {
+    public int findTargetSumWays99(int[] nums, int target) {
       int cnt = 0;
       int length = nums.length;
       Queue<Integer> queue = new ArrayDeque<>(1 << length);
@@ -96,56 +96,113 @@ public class TargetSum {
       return cnt;
     }
     
+    int ans = 0;
+    private int[] nums;
+    private int target;
+    HashMap<String, Integer> memo;
     
-    // 2.DFS   2^n 2^n
-    public int findTargetSumWays2(int[] nums, int target) {
-      int cnt = 0;
+    // dfs(使用全局变量维护)
+    public int findTargetSumWays9(int[] nums, int target) {
+      this.nums = nums;
+      this.target = target;
       
-      for (Integer sum : DFS(nums, 0)) {
+      dfs(0, 0);
+      
+      return ans;
+    }
+    
+    private void dfs(int i, int sum) {
+      if (i == nums.length) {
         if (sum == target) {
-          cnt++;
+          ans++;
         }
+        return;
       }
       
-      return cnt;
+      dfs(i + 1, sum + nums[i]);
+      dfs(i + 1, sum - nums[i]);
     }
     
-    private List<Integer> DFS(int[] nums, int i) {
-      int length = nums.length;
-      List<Integer> list = new ArrayList<>(1 << (length - i));
+    // dfs(接收返回值处理（后序遍历）)
+    public int findTargetSumWays8(int[] nums, int target) {
+      this.nums = nums;
+      this.target = target;
       
-      if (i == length - 1) {
-        list.add(nums[i]);
-        list.add(-nums[i]);
-      } else {
-        for (Integer sum : DFS(nums, i + 1)) {
-          list.add(sum + nums[i]);
-          list.add(sum - nums[i]);
-        }
+      return dfsWithRet(0, 0);
+    }
+    
+    private int dfsWithRet(int i, int sum) {
+      if (i == nums.length) {
+        return sum == target ? 1 : 0;
       }
       
-      return list;
+      return dfsWithRet(i + 1, sum + nums[i]) +
+          dfsWithRet(i + 1, sum - nums[i]);
     }
     
-    // 3.官解一：回溯  2^n n
-    private int count = 0;
-    
-    public int findTargetSumWays3(int[] nums, int target) {
-      backtrack(nums, target, 0, 0);
-      return count;
+    // 记搜
+    public int findTargetSumWays7(int[] nums, int target) {
+      this.nums = nums;
+      this.target = target;
+      memo = new HashMap<>();
+      
+      return dfsWithRetMemo(0, 0);
     }
     
-    private void backtrack(int[] nums, int target, int idx, int sum) {
-      if (idx == nums.length) {
-        if (sum == target) {
-          count++;
-        }
-      } else {
-        backtrack(nums, target, idx + 1, sum + nums[idx]);
-        backtrack(nums, target, idx + 1, sum - nums[idx]);
+    private int dfsWithRetMemo(int i, int sum) {
+      String key = i + "_" + sum;
+      if (memo.containsKey(key)) {
+        return memo.get(key);
       }
+      
+      if (i == nums.length) {
+        int value = sum == target ? 1 : 0;
+        memo.put(key, value);
+        return value;
+      }
+      
+      int value = dfsWithRetMemo(i + 1, sum + nums[i]) +
+          dfsWithRetMemo(i + 1, sum - nums[i]);
+      memo.put(key, value);
+      return value;
     }
     
+    /*
+     * ☆☆☆☆☆ 01背包（求方案数）[一维优化 + 常数优化]
+     * 问题转化：将数组分为两部分（正数堆s1/负数堆s2），则有：
+     *  ① s1+s2=sum
+     *  ② s1-s2=t
+     * ① 代入 ② 可得 s1=(sum+t)/2，s2=(sum-t)/2，我们取 min(s1,s2) 作为新的目标值 t（常数优化）
+     * 问题转化为求数组子集元素和为 t 的数目
+     *
+     * 特判（(sum-t)/2同理）：
+     *  - 0<=(sum+t)/2<=sum
+     *  - (sum+t)/2为整数
+     *
+     * 状态定义：f[i][j]表示考虑前 i 个元素，元素和为 j 的方案数
+     * 状态转移：f[i][j]=
+     *            （不选）f[i-1][j]+
+     *            （选取）f[i-1][j-nums[i-1]]
+     * 初始化　：f[0][0]=1
+     * 答案　　：f[n][t]
+     */
+    public int findTargetSumWays(int[] nums, int t) {
+      int sum = Arrays.stream(nums).sum();
+      if (sum < t || sum + t < 0 || (sum - t) % 2 != 0) {
+        return 0;
+      }
+      
+      t = (sum + (t > 0 ? -t : t)) / 2; // 常数优化
+      int[] f = new int[t + 1];
+      f[0] = 1;
+      for (int x : nums) {
+        // 倒序遍历（一维优化）
+        for (int j = t; j >= x; j--) {
+          f[j] += f[j - x];
+        }
+      }
+      return f[t];
+    }
   }
   // leetcode submit region end(Prohibit modification and deletion)
 }
