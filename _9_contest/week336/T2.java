8b927f86d0e15c396f239871cfa382487b8fdbb1
joey <joey.91@qq.com>
2023-03-12 12:07:04 +0800

    week336

--- _9_contest/week336/T2.java
+++ _9_contest/week336/T2.java
@@ -1,5 +1,7 @@
 package _9_contest.week336;
 
+import java.util.Arrays;
+
 //
 public class T2 {
   public static void main(String[] args) {
@@ -8,6 +10,53 @@ public class T2 {
   }
   
   class Solution {
-  
+    
+    public int maxScoreX(int[] nums) {
+      // Sort the array in descending order
+      Arrays.sort(nums);
+      // reverse(nums);
+      
+      // Initialize prefix sum array and score
+      // int[] prefix = new int[nums.length];
+      int score = 0;
+      
+      // Calculate prefix sums and score
+      for (int i = nums.length - 1, prefix = 0; i >= 0; i--) {
+        // if (i == nums.length - 1) {
+        //   prefix = nums[i];
+        // } else {
+        // }
+        prefix = prefix + nums[i];
+        if (prefix > 0) {
+          score++;
+        } else {
+          break;
+        }
+      }
+      
+      // Return the maximum score
+      return score;
+    }
+    
+    public int maxScore(int[] nums) {
+      Arrays.sort(nums);
+      
+      // int score = 0;
+      
+      long prefix = 0;
+      for (int i = nums.length - 1; i >= 0; i--) {
+        prefix += nums[i];
+        // if (prefix > 0) {
+        //   score++;
+        // } else {
+        //   break;
+        // }
+        if (prefix <= 0) {
+          return nums.length - 1 - i;
+        }
+      }
+      
+      return  nums.length;
+    }
   }
 }
--- _9_contest/week336/T3.java
+++ _9_contest/week336/T3.java
@@ -1,5 +1,7 @@
 package _9_contest.week336;
 
+import java.util.HashMap;
+
 //
 public class T3 {
   public static void main(String[] args) {
@@ -8,6 +10,18 @@ public class T3 {
   }
   
   class Solution {
-  
+    
+    public long beautifulSubarrays(int[] nums) {
+      long ans = 0;
+      HashMap<Integer, Integer> map = new HashMap<>();
+      int s = 0;
+      map.put(0, 1);
+      for (int x : nums) {
+        s ^= x;
+        ans += map.getOrDefault(s, 0);
+        map.merge(s, 1, Integer::sum);
+      }
+      return ans;
+    }
   }
 }
