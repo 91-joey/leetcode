9460334f2fab7b93ce43e4a04adf99428408fb83
joey <joey.91@qq.com>
2023-03-12 17:12:51 +0800

    6318. Minimum Time to Complete All Tasks

--- _9_contest/week336/T4.java
+++ _9_contest/week336/T4.java
@@ -1,6 +1,9 @@
 package _9_contest.week336;
 
-//
+import java.util.Arrays;
+import java.util.Comparator;
+
+// 6318. Minimum Time to Complete All Tasks
 public class T4 {
   public static void main(String[] args) {
     Solution solution = new T4().new Solution();
@@ -8,6 +11,35 @@ public class T4 {
   }
   
   class Solution {
+    // 排序 + 贪心 + 暴力
+    public int findMinimumTime(int[][] tasks) {
+      // 按右端点排序
+      Arrays.sort(tasks, Comparator.comparingInt(o -> o[1]));
+      // 某个整数时间点的电脑状态：运行（true）、关闭（false）
+      boolean[] run = new boolean[2001];
+      int ans = 0;
+      for (int[] task : tasks) {
+        int start = task[0];
+        int end = task[1];
+        int duration = task[2];
   
+        // 根据区间内的已有的电脑运行时间点，求待新增时间点
+        for (int i = start; i <= end; i++) {
+          if (run[i]) {
+            duration--;
+          }
+        }
+        // 贪心：尽量把新增的时间点安排在区间 [start,end] 的后缀上，这样下一个区间就能统计到更多已有的时间点。
+        for (int i = end; duration > 0; i--) {
+          if (!run[i]) {
+            run[i] = true;
+            duration--;
+            ans++;
+          }
+        }
+      }
+      
+      return ans;
+    }
   }
 }
